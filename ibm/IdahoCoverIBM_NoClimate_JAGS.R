7d60d272f4fc28a9cfa3b5cd061b54217c58d43f
Andrew Tredennick <atredenn@Andrews-iMac.local>
2014-07-01 13:41:07 -0600

    updated with multiple likelihoods

--- ibm/IdahoCoverIBM_NoClimate_JAGS.R
+++ ibm/IdahoCoverIBM_NoClimate_JAGS.R
@@ -5,11 +5,10 @@ model{
   varObs <- 1/varObsf
   varP <- 1/varPf
   
-  for(i in 1:3){
-    regVarf[i] ~ dgamma(.01,.01)
-  }
+  regVarf ~ dgamma(.01,.01)
   regVar <- 1/regVarf
   
+  #Random intercept and size effects
   for(i in 1:2){
     alphaMean[i] ~ dnorm(0,100)
     betaMean[i] ~ dnorm(0,100)
@@ -19,67 +18,81 @@ model{
   varAlpha <- 1/varAlphaf
   varBeta <- 1/varBetaf
   
-  for(i in 1:1){
-    gammaMean[i] ~ dnorm(0,100)
-    varGammaf[i] ~ dgamma(.01,.01)
-  }
+  #Random intercept for colonization
+  gammaMean ~ dnorm(0,100)
+  varGammaf ~ dgamma(.01,.01)
   varGamma <- 1/varGammaf
   
+  #Climate effects
+#   for(i in 1:4){
+#     alphaClim[i] ~ dnorm(0,100)
+#     betaClim[i] ~ dnorm(0,100)
+#     gammaClim[i] ~ dnorm(0,100)
+#   }
+  
+  #Draw random quadrat coefficients from mean distributions
   for(i in 1:nQuad){
     for(j in 1:2){
       alpha[i,j] ~ dnorm(alphaMean[j], varAlpha[j])
       beta[i,j] ~ dnorm(betaMean[j], varBeta[j])
     }
-    
-    for(j in 1:1){
-      gamma[i,j] ~ dnorm(gammaMean[j], varGamma[j])
-    }
+    gamma[i] ~ dnorm(gammaMean, varGamma)
     N0[i] ~ dunif(0,100) #initial value for N, it has to be between 0-100%
   }
   
   #Process models
   #initial conditions
   for(k in 1:nQuad){
-    S[1,k] <- 1/(1+exp(alpha[k,1]+alpha[k,2]*N0[k]))
+    logit(S[1,k]) <- alpha[k,1]+alpha[k,2]*N0[k]
     G[1,k] <- beta[k,1]+beta[k,2]*N0[k]
-    C[1,k] <- 1/(1+exp(gamma[k,1]))
+    logit(C[1,k]) <- gamma[k]
     
-    Sp[1,k] ~ dbern(S[1,k])
-    Gp[1,k] ~ dnorm(G[1,k], regVar[2])
-    Cp[1,k] ~ dbern(C[1,k])
-    
+    #     Sp[1,k] ~ dbern(S[1,k])
+    #     Gp[1,k] ~ dnorm(G[1,k], regVar)
+    #     Cp[1,k] ~ dbern(C[1,k])
+    #     
     N.pm[1,k] <- S[1,k]*G[1,k]
     N.p[1,k] <- ifelse(N.pm[1,k]==0, C[1,k]*0.01, N.pm[1,k])
     
-    N[1,k] ~ dlnorm(N.p[1,k], varP)
+    N[1,k] ~ dnorm(N.p[1,k], varP)
   }
   
   for(t in 2:nProc){
     for(k in 1:nQuad){
-      S[t,k] <- 1/(1+exp(alpha[k,1]+alpha[k,2]*N[t-1,k]))
+      logit(S[t,k]) <- alpha[k,1]+alpha[k,2]*N[t-1,k]
       G[t,k] <- beta[k,1]+beta[k,2]*N[t-1,k]
-      C[t,k] <- 1/(1+exp(gamma[k,1]))
+      logit(C[t,k]) <- gamma[k]
       
-      Sp[t,k] ~ dbern(S[t,k])
-      Gp[t,k] ~ dnorm(G[t,k], regVar[2])
-      Cp[t,k] ~ dbern(C[t,k])
+      #       Sp[t,k] ~ dbern(S[t,k])
+      #       Gp[t,k] ~ dnorm(G[t,k], regVar)
+      #       Cp[t,k] ~ dbern(C[t,k])
       
       N.pm[t,k] <- S[t,k]*G[t,k]
       N.p[t,k] <- ifelse(N.pm[t,k]==0, C[t,k]*0.01, N.pm[t,k])
       
-      N[t,k] ~ dlnorm(N.p[t,k], varP)
+      N[t,k] ~ dnorm(N.p[t,k], varP)
     }
   }
   
-  #Likelihood
+  #Likelihoods
   for(i in 1:nObs){
     y[i] ~ dnorm(N[timeN[i],quadN[i]], varObs)
+  }
+  for(i in 1:nCol){
+    yCol[i] ~ dbern(C[timeNcol[i], quadNcol[i]])
+  }
+  for(i in 1:nSurv){
+    ySurv[i] ~ dbern(S[timeNsurv[i], quadNsurv[i]])
+  }
+  for(i in 1:nGrow){
+    yGrow[i] ~ dnorm(G[timeNgrow[i], quadNgrow[i]], regVar)
   }
   
   #Derived quantities
   #calculate mean over quads for each year
-  qCount <- 1
   for(i in 1:nProc){
     muN[i] <- mean(N[i,])
   }
+  
 }
+
