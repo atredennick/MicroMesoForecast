016b56578b1d945638cfbe02c4f80e71f0b81441
Andrew Tredennick <atredenn@gmail.com>
2014-10-27 17:51:17 -0600

    added HECO script; clim vars not converging

---
+++ montana/quadBM/vitalRateRegressions/MCMC_FinalModels/growthHECO_JAGS.R
@@ -0,0 +1,29 @@
+model{
+  #growth process and likelihood
+  for(i in 1:nObs){
+    logit(gMu[i]) <- intYr[yrs[i]] + intG[grp[i]] + size[i]*beta[yrs[i]] + temp1*TmeanSpr1[i] + temp2*TmeanSpr2[i] 
+    p[i] <- gMu[i] * tau
+    q[i] <- (1 - gMu[i]) * tau
+    Y[i] ~ dbeta(p[i], q[i])
+  }
+  
+  #priors
+  for(g in 1:nGrp){
+    intG[g] ~ dnorm(0, intVarG)
+  }
+  
+  for(y in 1:nYrs){
+    beta[y] ~ dnorm(betaMu, betaVar)
+    intYr[y] ~ dnorm(interceptMu, intVarY)
+  }
+  
+  temp1 ~ dunif(-10,10)
+  temp2 ~ dunif(-10,10)
+  interceptMu ~ dnorm(0, .001)
+  betaMu ~ dnorm(0, .001)
+  betaVar ~ dgamma(0.001, 0.001)
+  intVarY ~ dgamma(0.001, 0.001)
+  intVarG ~ dgamma(2, 0.5)
+  t0 ~ dnorm(0, .01) 
+  tau <- exp(t0)
+}
---
+++ montana/quadBM/vitalRateRegressions/MCMC_FinalModels/growthHECO_MCMC.R
@@ -0,0 +1,83 @@
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
+# Set working directory to location of this source file #
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
+
+#clear everything, just to be safe 
+rm(list=ls(all=TRUE))
+
+doSpp <- "HECO"
+
+#load libraries
+library(rjags)
+library(coda)
+library(ggmcmc)
+
+#bring in data
+allD <- read.csv("../../../speciesData/quadAllCover.csv")
+allD <- allD[,2:ncol(allD)] #get rid of X ID column
+sppList <- as.character(unique(allD$Species))
+sppD <- subset(allD, Species==doSpp)
+
+climD <- read.csv("../../../weather/Climate.csv")
+
+# create lag cover variable
+tmp=sppD[,c("quad","year","totCover")]
+tmp$year=tmp$year+1
+names(tmp)[3]="lag.cover"
+sppD=merge(sppD,tmp,all.x=T)
+
+# merge in climate data
+sppD$climYear=sppD$year+1900-1  
+sppD=merge(sppD,climD,by.x="climYear",by.y="year")
+
+#Growth observations
+growD <- subset(sppD,lag.cover>0 & totCover>0)
+growD$yearID <- growD$year #for random year offset on intercept
+growD$group <- substring(growD$quad, 1, 1)
+growD$percCover <- growD$totCover/10000
+growD$percLagCover <- growD$lag.cover/10000
+
+
+####
+#### Set up data structure for JAGS
+####
+nGrp <- length(unique(growD$group))
+nYrs <- length(unique(growD$year))
+nObs <- nrow(growD)
+size <- growD$percLagCover
+Y <- growD$percCover
+yrs <- (growD$year)-32
+grp <- as.numeric(as.factor(growD$group))
+TmeanSpr1 <- growD$TmeanSpr1
+TmeanSpr2 <- growD$TmeanSpr2
+
+####
+#### Run MCMC
+####
+iterations <- 10000
+dataJ <- list(nGrp=nGrp, nYrs=nYrs, nObs=nObs, size=size, Y=Y, yrs=yrs, grp=grp,
+              TmeanSpr1=TmeanSpr1, TmeanSpr2=TmeanSpr2)
+mod <- jags.model(paste("growth", doSpp, "_JAGS.R", sep=""), data=dataJ, n.chains=3, n.adapt=5000)
+update(mod, n.iter = 10000)
+out <- coda.samples(mod, c("beta", "intYr", "intG", "temp1", "temp2"),
+                    n.iter=iterations, n.thin=10)
+
+####
+#### Check for convergence
+####
+gelman.diag(out)
+# heidel.diag(out)
+# gelman.plot(out)
+# plot(out)
+
+####
+#### Convert to dataframe for export and get other summaries
+####
+outC <- rbind(out[[1]][(iterations-999):iterations,], 
+              out[[2]][(iterations-999):iterations,], 
+              out[[3]][(iterations-999):iterations,])
+
+saveRDS(outC, file = paste(doSpp, "_GrowthParamsMCMC.rds", sep=""))
+
+
+
