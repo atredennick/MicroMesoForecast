ee1eaff8277871c4754a0a2c64e9f80bf2e31c9c
--global user.email "atredenn@gmail.com <atredenn@gmail.com>
2014-10-30 17:25:36 -0600

    wd alert added

--- montana/quadBM/vitalRateRegressions/MCMC_FinalModels/survival/survivalWrapper.R
+++ montana/quadBM/vitalRateRegressions/MCMC_FinalModels/survival/survivalWrapper.R
@@ -1,5 +1,9 @@
 #wrapper for survival MCMC estimation
 
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
+# Set working directory to location of this source file #
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
+
 source("survivalAllSpp_MCMC.R")
 source("survivalAllSpp_CONSTANT_MCMC.R")
 source("survivalAllSpp_CLIMATE_MCMC.R")
