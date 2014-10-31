4db9d45ed84e8d7de7343df741c06858585a6dde
Andrew Tredennick <atredenn@gmail.com>
2014-10-31 14:57:53 -0600

    added a species hierarchy MCMC version

--- montana/quadBM/vitalRateRegressions/MCMC_FinalModels/survival/colonizationAllSpp_JAGS.R
+++ montana/quadBM/vitalRateRegressions/MCMC_FinalModels/survival/colonizationAllSpp_JAGS.R
@@ -1,24 +1,27 @@
 model{
   #likelihood and process model
   for(i in 1:nObs){
-    logit(p[i]) <- interceptMu 
+    logit(p[i]) <- intercept[spp[i]] + temp1[spp[i]]*TmeanSpr1[i] + temp2[spp[i]]*TmeanSpr2[i] + rain1[spp[i]]*ppt1[i] + rain2[spp[i]]*ppt2[i]  
     C[i] ~ dbern(p[i])
   }
   
-#   #priors
-#   for(g in 1:nGrp){
-#     intG[g] ~ dnorm(0, intVarG)
-#   }
-#   
-#   for(y in 1:nYrs){
-#     intYr[y] ~ dnorm(interceptMu, intVarY)
-#   }
+  #priors
+  for(s in 1:nSpp){
+    intercept[s] ~ dnorm(interceptMu, intVar)
+    temp1[s] ~ dnorm(temp1Mu, temp1Var)
+    temp2[s] ~ dnorm(temp2Mu, temp2Var)
+    rain1[s] ~ dnorm(rain1Mu, rain1Var)
+    rain2[s] ~ dnorm(rain2Mu, rain2Var)
+  }
   
-  temp1 ~ dnorm(0,1e-6)
-  temp2 ~ dnorm(0,1e-6)
-  rain1 ~ dnorm(0,1e-6)
-  rain2 ~ dnorm(0,1e-6)
+  temp1Mu ~ dnorm(0,1e-6)
+  temp2Mu ~ dnorm(0,1e-6)
+  rain1Mu ~ dnorm(0,1e-6)
+  rain2Mu ~ dnorm(0,1e-6)
   interceptMu ~ dnorm(0, 0.0001)
-  intVarY ~ dgamma(0.001, 0.001)
-  intVarG ~ dgamma(2, 0.5) 
+  intVar ~ dgamma(0.001, 0.001)
+  temp1Var ~ dgamma(0.001, 0.001)
+  temp2Var ~ dgamma(0.001, 0.001)
+  rain1Var ~ dgamma(0.001, 0.001)
+  rain2Var ~ dgamma(0.001, 0.001)
 }
