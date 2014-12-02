438a2cd046fb0b4436040c684dd320f5df37c5ed
Andrew Tredennick <atredenn@gmail.com>
2014-12-01 17:36:49 -0700

    mcmc results 1

--- montana/ipm/vitalRateRegs/growth/growthWrapper.R
+++ montana/ipm/vitalRateRegs/growth/growthWrapper.R
@@ -2,6 +2,6 @@
 #### Wrapper script to call each growth model
 ####
 
-source("growthAllSpp_MCMC.R")
+# source("growthAllSpp_MCMC.R")
 source("growthAllSppCLIMATE_MCMC.R")
 source("growthAllSppCONSTANT_MCMC.R")
