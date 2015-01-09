5ce2d13ec78820c885f486e4e0b970f96433295b
Andrew Tredennick <atredenn@gmail.com>
2015-01-08 17:17:54 -0700

    tested

--- montana/ipm/vitalRateRegs/recruitment/import2ipm.R
+++ montana/ipm/vitalRateRegs/recruitment/import2ipm.R
@@ -3,63 +3,66 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
 #Several arguments are required
-#' @param doYear  Specific climate year to pull year random effects
-#' @param mcDraw  A numeric scalar or vector for the row(s) of MCMC to draw parameters from 
+#' @param doYear  Specific climate year to pull year random effects (19xx)
+#' @param mcDraw  A numeric scalar for the row(s) of MCMC to draw parameters from 
 #' @param doSpp   A character scalar for the current speicies
-# @param group   A numeric scalar with group indicator
+#' @param group   A numeric scalar with group indicator
 
 library(reshape2)
 library(plyr)
 
-sppList=c("ARTR","HECO","POSE","PSSP") #in here for now; remove after testing
+sppList=sort(c("BOGR","HECO","PASM","POSE")) #in here for now; remove after testing
 
 ####
 #### Read in full MCMC output and format as data frame
 ####
 MCMC <- readRDS("recruitmentParamsMCMC.rds")
-prec2 <- melt(MCMC)
-prec2$Spp <- c(rep(rep(sppList, each=3000), times=13),
+pRec2 <- melt(MCMC)
+pRec2$Spp <- c(rep(sppList, each=3000),
                 rep(rep(sppList, each=3000), times=6),
                 rep(rep(sppList, each=3000), times=13),
-                rep(rep(sppList, each=3000), times=5))
-prec2$Coef <- c(rep("beta", times=3000*4*13),
+                rep(rep(sppList, each=3000), times=6))
+pRec2$Coef <- c(rep("dd", times=3000*4),
                  rep("gInt", times=6*4*3000),
                  rep("intYr", times=4*3000*13),
-                 rep("nb", times=4*3000),
                  rep("rain1", times=4*3000),
                  rep("rain2", times=4*3000),
                  rep("temp1", times=4*3000),
-                 rep("temp2", times=4*3000))
-colnames(prec2)[1] <- "Iter"
-prec <- prec2[,c(1,3:5)]; rm(prec2)
-precAll <- subset(prec, Coef=="gInt"|Coef=="nb"|Coef=="rain1"|Coef=="rain2"|Coef=="temp1"|Coef=="temp2")
-precYrs <- subset(prec, Coef=="beta" | Coef=="intYr")
+                 rep("temp2", times=4*3000),
+                rep("theta", times=4*3000),
+                rep("u", times=4*3000))
+colnames(pRec2)[1] <- "Iter"
+pRec <- pRec2[,c(1,3:5)]; rm(pRec2)
+pRecAll <- subset(pRec, Coef=="gInt"|Coef=="dd"|Coef=="rain1"|Coef=="rain2"|Coef=="temp1"|Coef=="temp2"|Coef=="theta"|Coef=="u")
+pRecYrs <- subset(pRec, Coef=="intYr")
 years <- unique(allD$year)[2:14]+1900
-precYrs$Year <- c(rep(rep(years, each=3000), each=4),
-                   rep(rep(years, each=3000), each=4))
+pRecYrs$Year <- c(rep(rep(years, each=3000), each=4))
 
 ####
 #### Now get subset defined by parameters; in a function
 ####
 getRecCoefs <- function(doYear, mcDraw, doSpp){
   #First do coefficients with random year effects
-  recNowYr <- subset(precYrs, Spp==doSpp & Year==doYear & Iter==mcDraw)
+  recNowYr <- subset(pRecYrs, Spp==doSpp & Year==doYear & Iter==mcDraw)
   iID <- which(recNowYr$Coef=="intYr")
   intercept <- recNowYr$value[iID]
-  sID <- which(recNowYr$Coef=="beta")
-  size <- recNowYr$value[sID]
   
-  #Now do climate and competition fixed effects
-  recNow <- subset(precAll, Spp==doSpp & Iter==mcDraw)
+  #Now do group, climate, and competition fixed effects
+  recNow <- subset(pRecAll, Spp==doSpp & Iter==mcDraw)
   cID <- which(recNow$Coef=="rain1"|recNow$Coef=="rain2"|recNow$Coef=="temp1"|recNow$Coef=="temp2")
   climEffs <- recNow$value[cID]
-  dd <- recNow$value[which(recNow$Coef=="nb")]
+  dd <- recNow$value[which(recNow$Coef=="dd")]
+  gID <- which(recNow$Coef=="gInt")
+  intG <- recNow$value[gID[group]]
+  u <- recNow$value[which(recNow$Coef=="u")]
+  theta <- recNow$value[which(recNow$Coef=="theta")]
   
   #Collate all parameters for output
   Rpars=list(intcpt=intercept, 
-             slope=size,
-             nb=dd,
-             clim=climEffs)
-  
+             grpInt=intG,
+             dd=dd,
+             clim=climEffs,
+             u=u,
+             theta=theta)
   out(Rpars)
 }
