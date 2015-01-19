d740358059658f99a713cdf4bc530d345231079a
--global user.email "atredenn@gmail.com <atredenn@gmail.com>
2015-01-19 10:33:04 -0700

    updated interactions

--- montana/quadBM/vitalRateRegressions/truncNormModel/growthAllSppHierarchy_MCMC.R
+++ montana/quadBM/vitalRateRegressions/truncNormModel/growthAllSppHierarchy_MCMC.R
@@ -84,8 +84,8 @@ X <- log(growD$percLagCover)
 ####
 #### Run MCMC
 ####
-iterations <- 5000
-adapt <- 1000
+iterations <- 50000
+adapt <- 10000
 dataJ <- list(nGrp=nGrp, nYrs=nYrs, nObs=nObs, C=C, X=X, yrs=yrs, grp=grp,
               TmeanSpr1=TmeanSpr1, TmeanSpr2=TmeanSpr2, ppt1=ppt1, ppt2=ppt2, spp=spp, nSpp=nSpp)
 mod <- jags.model("growthAllSpp_JAGS.R", data=dataJ, n.chains=3, n.adapt=adapt)
@@ -98,33 +98,33 @@ out <- coda.samples(mod, c("intYr", "intercept", "beta", "betaSpp", "intG", "tem
 ####
 #### Check for convergence
 ####
-# gelmDiag <- gelman.diag(out)
+gelmDiag <- gelman.diag(out)
 # # heidel.diag(out)
 # # gelman.plot(out)
 # 
-# # pdf("growthOutPlots.pdf")
-# # plot(out, auto.layout=FALSE)
-# # dev.off()
+pdf("growthOutPlots.pdf")
+plot(out, auto.layout=FALSE)
+dev.off()
 # 
 # ####
 # #### Convert to dataframe for export and get other summaries
 # ####
-# outC <- rbind(out[[1]][(iterations-999):iterations,], 
-#               out[[2]][(iterations-999):iterations,], 
-#               out[[3]][(iterations-999):iterations,])
+outC <- rbind(out[[1]][(iterations-999):iterations,], 
+              out[[2]][(iterations-999):iterations,], 
+              out[[3]][(iterations-999):iterations,])
 # 
 outStat <- as.data.frame(summary(out)$stat)
-# outQuant <- as.data.frame(summary(out)$quantile)
-# outDeviance <- as.data.frame(summary(dic$deviance, mean)$stat)
+outQuant <- as.data.frame(summary(out)$quantile)
+outDeviance <- as.data.frame(summary(dic$deviance, mean)$stat)
 # 
 sppNames <- c(rep(sppList, 13+6+13+4+1+1+1))
 outStat$species <- sppNames
-# outQuant$species <- sppNames
+outQuant$species <- sppNames
 # 
-# saveRDS(outC, file = "growthParamsMCMC.rds")
-# write.csv(gelmDiag[[1]], file="growthGelman.csv")
+saveRDS(outC, file = "growthParamsMCMC.rds")
+write.csv(gelmDiag[[1]], file="growthGelman.csv")
 write.csv(outStat, file="growthStats.csv")
-# write.csv(outQuant, file="growthQuants.csv")
+write.csv(outQuant, file="growthQuants.csv")
 # # write.csv(outDeviance, file="growthDeviance.csv")
 # 
 # 
