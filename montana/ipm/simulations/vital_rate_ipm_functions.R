1a09c9881a150fc1084329b103a1180c5638dcb2
--global user.email "atredenn@gmail.com <atredenn@gmail.com>
2015-02-06 15:37:14 -0700

    added climate effects to regressions

--- montana/ipm/simulations/vital_rate_ipm_functions.R
+++ montana/ipm/simulations/vital_rate_ipm_functions.R
@@ -1,10 +1,9 @@
 ####
 #### Growth function
 ####
-G <- function(v,u,W,Gpars,doYear,doSpp){
-  mu <- Gpars$intcpt[doSpp]+Gpars$intcpt.yr[doYear,doSpp]+
-        (Gpars$slope[doSpp]+Gpars$slope.yr[doYear,doSpp])*u+
-        W*Gpars$nb[doSpp]
+G <- function(v,u,W,Gpars,doYear,doSpp,climate){
+  mu <- Gpars$intcpt[doSpp]+Gpars$intG[doSpp]+
+        Gpars$slope[doSpp]*u+W*Gpars$nb[doSpp]+sum(Gpars$clim[doSpp,]*climate)
   
   sigma2 <- Gpars$sigma2.a[doSpp]*exp(Gpars$sigma2.b[doSpp]*mu)
   out <- dnorm(v,mu,sqrt(sigma2))
@@ -15,9 +14,9 @@ G <- function(v,u,W,Gpars,doYear,doSpp){
 #### Survival function: probability an individual of size 
 #### u survives  (u is on log scale)
 ####
-S <- function(u,W,Spars,doYear,doSpp){
-  mu <- Spars$intcpt[doSpp]+Spars$intcpt.yr[doYear,doSpp]+(Spars$slope[doSpp]+Spars$slope.yr[doYear,doSpp])*u+
-        W*Spars$nb[doSpp]
+S <- function(u,W,Spars,doYear,doSpp,climate){
+  mu <- Spars$intcpt[doSpp]+Spars$intG[doSpp]+Spars$slope[doSpp]*u+
+        W*Spars$nb[doSpp]+sum(Spars$clim[doSpp,]*climate)
   return(inv.logit(mu))
 }
 
@@ -26,14 +25,15 @@ S <- function(u,W,Spars,doYear,doSpp){
 #### Number of recruits per area produced 
 #### cover is stored in absolute area (cm^2)
 ####
-get_rpa=function(Rpars,cover){
+get_rpa=function(Rpars,cover,climate){
   # cover is in m^2 per m^2; convert to % scale:
   cover2=cover*100
   # calculate recruits
   n_spp=length(cover)
   mu=rep(NA,n_spp)
   for(i in 1:n_spp){
-    mu[i]=cover2[i]*exp(Rpars$intcpt[i]+sqrt(cover2[i])*Rpars$dd[i]) 
+    mu[i]=cover2[i]*exp(Rpars$intcpt[i]+sqrt(cover2[i])*Rpars$dd[i]+
+                          Rpars$grpInt[i]+(Rpars$clim[i,]*climate)) 
   }
   if(sum(is.na(mu))>0) browser() # stop for errors
   rpa=mu/(cover*A)  # convert from number recruits to recruits per cm^2
