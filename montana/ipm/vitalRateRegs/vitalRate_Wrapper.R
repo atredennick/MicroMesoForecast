e9979cc516c9552b42d57c68e0a47f7432c7cfbe
--global user.email "atredenn@gmail.com <atredenn@gmail.com>
2015-02-09 16:21:22 -0700

    changed some files in the wrapper

--- montana/ipm/vitalRateRegs/vitalRate_Wrapper.R
+++ montana/ipm/vitalRateRegs/vitalRate_Wrapper.R
@@ -9,17 +9,24 @@
 ####
 #### Growth
 ####
+print("STARTING GROWTH")
 setwd("./growth/")
 # source("growthWrapper.R")
+source("growthAllSpp_MCMC.R")
+print("DONE WITH GROWTH")
 
 ####
 #### Survival
 ####
+print("STARTING SURVIVAL")
 setwd("../survival/")
-source("survivalWrapper.R")
+source("survivalAllSpp_MCMC.R")
+print("DONE WITH SURVIVAL")
 
 ####
 #### Colonization
 ####
+print("STARTING RECRUITMENT")
 setwd("../recruitment/")
-source("recruitmentWrapper.R")
+source("recruitmentAllSpp_MCMC.R")
+print("DONE WITH RECRUITMENT")
