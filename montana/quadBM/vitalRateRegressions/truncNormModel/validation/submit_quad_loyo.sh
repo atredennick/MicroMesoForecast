46ef6ed86927251d440737ba065398c66b0aab86
Andrew Tredennick <atredenn@gmail.com>
2015-03-23 11:26:27 -0600

    Update bash script with correct file path.

--- montana/quadBM/vitalRateRegressions/truncNormModel/validation/submit_quad_loyo.sh
+++ montana/quadBM/vitalRateRegressions/truncNormModel/validation/submit_quad_loyo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#SBATCH --array=1933-1945
 #SBATCH --job-name=R_quadLOYO_job
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
@@ -9,10 +10,8 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=atredenn@gmail.com
 
-#SBATCH --array=1933-1945
-
 . /rc/tools/utils/dkinit
 use JAGS
 reuse -q R
 
-R CMD BATCH $HOME/quadmodel/growthAllSppHierarchy_MCMC_LOYO_$SLURM_ARRAY_TASK_ID.R
+R CMD BATCH $HOME/validation/growthAllSppHierarchy_MCMC_LOYO_$SLURM_ARRAY_TASK_ID.R
