cc740afdee9a8708c480cd7b77e807b9b704a96b
Andrew Tredennick <atredenn@gmail.com>
2015-03-24 13:18:21 -0600

    Initial commit of script for collating validation results.

---
+++ montana/quadBM/simulations/collate_validation_sims.R
@@ -0,0 +1,14 @@
+##  This script collates the leave-one-year-out validation simulations.
+
+##  Author: Andrew Tredennick
+##  Email:  atredenn@gmail.com
+
+####
+#### Load libraries ----------------------------------------
+####
+library('reshape2'); library('plyr'); library('ggplot2')
+
+
+####
+####  Read in simulationr results 1-by-1 and combine -------
+####
