30ea885bf900a3a4cdb15bbad59e3d8134750c08
Andrew Tredennick <atredenn@gmail.com>
2015-03-24 13:34:07 -0600

    Add boxplot of results. Need to calc residuals still.

--- montana/quadBM/simulations/collate_validation_sims.R
+++ montana/quadBM/simulations/collate_validation_sims.R
@@ -10,5 +10,22 @@ library('reshape2'); library('plyr'); library('ggplot2')
 
 
 ####
-####  Read in simulationr results 1-by-1 and combine -------
+####  Read in simulation results 1-by-1 and combine --------
 ####
+all_files <- list.files(path = "./validation_results/")
+num_files <- length(all_files)
+data_list <- list()
+for(file in 1:num_files){
+  now_file <- paste("./validation_results/", all_files[file], sep="")
+  data_list[[file]] <- readRDS(now_file)
+}
+
+validation_sims <- melt(data_list, id.vars = c("quad", "species", "predicted_year", "sim")) 
+#TODO: bring in observed data to calculate residuals
+
+####
+####  Quick plot -------------------------------------------
+####
+ggplot(validation_sims)+
+  geom_boxplot(aes(x=as.character(predicted_year), y=value*100))+
+  facet_wrap("species", scales = "free")
