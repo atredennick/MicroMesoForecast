2f6b6445f6b3f6caa0bcb9ada71b4ae88c0f847c
Andrew Tredennick <atredenn@gmail.com>
2015-05-09 11:04:01 -0600

    cleaning up files and starting STAN

---
+++ montana/ipm/vitalRateRegs/survival/cache/survivalAllSpp_JAGS.R
@@ -0,0 +1,38 @@
+model{
+  #process model and likelihood
+  for(i in 1:nObs){
+    logit(p[i]) <- intYr[spp[i],yrs[i]] + intG[spp[i],grp[i]] + beta[spp[i],yrs[i]]*X[i] + nb[spp[i]]*crowd[i] + temp1[spp[i]]*TmeanSpr1[i] + temp2[spp[i]]*TmeanSpr2[i] + rain1[spp[i]]*ppt1[i] + rain2[spp[i]]*ppt2[i]
+    Y[i] ~ dbern(p[i])
+  }
+  
+  #priors
+  for(s in 1:nSpp){
+    betaSpp[s] ~ dnorm(0, 1e-6)
+    nb[s] ~ dnorm(0, 1e-6)
+    temp1[s] ~ dnorm(temp1Mu, temp1Var)
+    temp2[s] ~ dnorm(temp2Mu, temp2Var)
+    rain1[s] ~ dnorm(rain1Mu, rain1Var)
+    rain2[s] ~ dnorm(rain2Mu, rain2Var)
+    intercept[s] ~ dnorm(0, 1e-6)
+    intVaryY[s] ~ dgamma(0.001, 0.001)
+    betaVar[s] ~ dgamma(0.001, 0.001)
+    error[s] ~ dgamma(0.001, 0.001)
+    intVarG[s] ~ dgamma(2, 0.5) 
+    for(y in 1:nYrs){
+      intYr[s,y] ~ dnorm(intercept[s], intVaryY[s])
+      beta[s,y] ~ dnorm(betaSpp[s], betaVar[s])
+    }
+    for(g in 1:nGrp){
+      intG[s,g] ~ dnorm(0, intVarG[s])
+    }
+  }
+  
+  temp1Mu ~ dnorm(0,1e-6)
+  temp2Mu ~ dnorm(0,1e-6)
+  rain1Mu ~ dnorm(0,1e-6)
+  rain2Mu ~ dnorm(0,1e-6)
+  temp1Var ~ dgamma(0.001, 0.001)
+  temp2Var ~ dgamma(0.001, 0.001)
+  rain1Var ~ dgamma(0.001, 0.001)
+  rain2Var ~ dgamma(0.001, 0.001)
+}
