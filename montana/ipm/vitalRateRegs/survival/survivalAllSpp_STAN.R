5f654cf859ca74619476e87b07ce6a95a8bf9ad9
Andrew Tredennick <atredenn@gmail.com>
2015-05-12 16:22:04 -0600

    takes out line hard coding species index

--- montana/ipm/vitalRateRegs/survival/survivalAllSpp_STAN.R
+++ montana/ipm/vitalRateRegs/survival/survivalAllSpp_STAN.R
@@ -13,8 +13,9 @@ args <- commandArgs(trailingOnly = F)
 myargument <- args[length(args)]
 myargument <- sub("-","",myargument)
 do_species <- as.numeric(myargument)
-do_species <- 1
 sppList=sort(c("BOGR","HECO","PASM","POSE"))
+
+
 ####
 #### Read in data by species and make one long data frame -------------
 ####
