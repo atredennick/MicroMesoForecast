eafc3a6bcb44a925b5008598c37e3995f4aa487f
Andrew Tredennick <atredenn@gmail.com>
2015-05-12 16:15:50 -0600

    adds covariate interactions and sets up script for HPC in array

--- montana/ipm/vitalRateRegs/survival/survivalAllSpp_STAN.R
+++ montana/ipm/vitalRateRegs/survival/survivalAllSpp_STAN.R
@@ -33,8 +33,8 @@ outD <- data.frame(X=NA,
                    allEdge=NA,
                    species=NA)
 
-# data_path <- "../../../speciesData/" #on local machine
-data_path <- "speciesData/" #on HPC server
+data_path <- "../../../speciesData/" #on local machine
+# data_path <- "speciesData/" #on HPC server
 
 for(spp in 1:length(sppList)){
   doSpp <- sppList[spp]
@@ -51,8 +51,8 @@ for(spp in 1:length(sppList)){
 
 survD <- outD[2:nrow(outD),]
 
-# climD <- read.csv("../../../weather/Climate.csv") #on local machine
-climD <- read.csv("Climate.csv") #on HPC server
+climD <- read.csv("../../../weather/Climate.csv") #on local machine
+# climD <- read.csv("Climate.csv") #on HPC server
 clim_vars <- c("pptLag", "ppt1", "ppt2", "TmeanSpr1", "TmeanSpr2")
 climD[,clim_vars] <- scale(climD[,clim_vars], center = TRUE, scale = TRUE)
 climD$year <- climD$year-1900
@@ -75,13 +75,11 @@ colnames(crowd) <- c("W", "X", "species")
 survD_all <- merge(survD, crowd, by=c("species", "X"))
 
 #try glm
-# fit final mixed effect model: based on what?
-# survD <- subset(survD_all, species=="BOGR")
 # library(lme4)
-# outlm=glmer(survives~log(area)+W+pptLag+ppt1+TmeanSpr1+ 
-#            ppt2+TmeanSpr2+
-#            ppt1:TmeanSpr1+ppt2:TmeanSpr2+
-#            (1|Group) + (log(area)|year),data=subset(survD, species=="BOGR"),
+# outlm=glmer(survives~log(area)*W+log(area)+pptLag+log(area)*ppt1+log(area)*TmeanSpr1+ 
+#               log(area)*ppt2+log(area)*TmeanSpr2+
+#               ppt1:TmeanSpr1+ppt2:TmeanSpr2+
+#            (1|Group) + (log(area)|year),data=subset(survD_all, species==sppList[4]),
 #            family=binomial) 
 # summary(outlm)
 
@@ -96,7 +94,7 @@ data{
   int<lower=0,upper=1> Y[N]; // observation vector
   matrix[N,Covs] C; // climate matrix
   vector[N] X; // size vector
-  vector[N] W; // crowding vector
+  matrix[N,2] W; // crowding matrix
 }
 parameters{
   real a_mu;
@@ -104,7 +102,7 @@ parameters{
   real b1_mu;
   vector[Yrs] b1;
   vector[Covs] b2;
-  real w;
+  vector[2] w;
   vector[G] gint;
   real<lower=0> sig_a;
   real<lower=0> sig_b1;
@@ -113,9 +111,11 @@ parameters{
 transformed parameters{
   real mu[N];
   vector[N] climEff;
+  vector[N] crowdEff;
   climEff <- C*b2;
+  crowdEff <- W*w;
   for(n in 1:N){
-    mu[n] <- inv_logit(a[yid[n]] + gint[gid[n]] + b1[yid[n]]*X[n] + w*W[n] + climEff[n]);
+    mu[n] <- inv_logit(a[yid[n]] + gint[gid[n]] + b1[yid[n]]*X[n] + crowdEff[n] + climEff[n]);
   }
 }
 model{
@@ -141,24 +141,24 @@ model{
 }
 "
 
-# rng_seed <- 123
-
-## Loop through species and fit model
-# library(lme4)
-# big_list <- list()
-
-## Compile model outside of loop
-survD <- subset(survD_all, species==sppList[do_species])
+## Compile model 
+survD <- subset(survD_all, species==sppList[3])
 clim_covs <- survD[,c("pptLag", "ppt1", "ppt2", "TmeanSpr1", "TmeanSpr2")]
 clim_covs$inter1 <- clim_covs$ppt1*clim_covs$TmeanSpr1
 clim_covs$inter2 <- clim_covs$ppt2*clim_covs$TmeanSpr2
+clim_covs$sizepptLag <- clim_covs$pptLag*log(survD$area)
+clim_covs$sizeppt1 <- clim_covs$ppt1*log(survD$area)
+clim_covs$sizeppt2 <- clim_covs$ppt2*log(survD$area)
+clim_covs$sizetemp1 <- clim_covs$TmeanSpr1*log(survD$area)
+clim_covs$sizetemp2 <- clim_covs$TmeanSpr2*log(survD$area)
 groups <- as.numeric(survD$Group)
 G <- length(unique(survD$Group))
 Yrs <- length(unique(survD$year))
+W <- cbind(survD$W, survD$W*log(survD$area))
 
 datalist <- list(N=nrow(survD), Yrs=Yrs, yid=(survD$year-31),
                  Covs=length(clim_covs), Y=survD$survives, X=log(survD$area),
-                 C=clim_covs, W=survD$W, G=G, gid=groups)
+                 C=clim_covs, W=W, G=G, gid=groups)
 pars=c("a_mu", "a", "b1_mu",  "b1", "b2",
        "w", "gint")
 mcmc_samples <- stan(model_code=model_string, data=datalist,
@@ -167,24 +167,17 @@ mcmc_samples <- stan(model_code=model_string, data=datalist,
 ## Set reasonable initial values for three chains
 inits <- list()
 inits[[1]] <- list(a_mu=0, a=rep(0,Yrs), b1_mu=0.01, b1=rep(0.01,Yrs),
-                   gint=rep(0,G), w=-0.05, sig_b1=0.5, sig_a=0.5,
+                   gint=rep(0,G), w=c(-0.05,-0.05), sig_b1=0.5, sig_a=0.5,
                    sig_G=0.5, b2=rep(0,length(clim_covs)))
 inits[[2]] <- list(a_mu=-0.1, a=rep(-0.1,Yrs), b1_mu=0.1, b1=rep(0.1,Yrs),
-                   gint=rep(-0.1,G), w=-0.1, sig_b1=0.2, sig_a=0.2,
+                   gint=rep(-0.1,G), w=c(-0.1,.-0.1), sig_b1=0.2, sig_a=0.2,
                    sig_G=0.2, b2=rep(0.1,length(clim_covs)))
 inits[[3]] <- list(a_mu=0.5, a=rep(0.5,Yrs), b1_mu=0.5, b1=rep(0.5,Yrs),
-                   gint=rep(-1,G), w=-0.5, sig_b1=0.1, sig_a=0.1,
+                   gint=rep(-1,G), w=c(-0.5,-0.5), sig_b1=0.1, sig_a=0.1,
                    sig_G=0.1, b2=rep(-1,length(clim_covs)))
 
-# datalist <- list(N=nrow(survD), Yrs=Yrs, yid=(survD$year-31),
-#                  Covs=length(clim_covs), Y=survD$survives, X=log(survD$area),
-#                  C=clim_covs, W=survD$W, G=G, gid=groups)
-# pars=c("a_mu", "a", "b1_mu",  "b1", "b2",
-#        "w", "gint")
 
-fitted <- stan(fit=mcmc_samples, data=datalist, pars=pars,
-               chains=1, iter=100, warmup=25, init=inits)
-
+##  Run MCMC in parallel
 rng_seed <- 123
 sflist <-
   mclapply(1:3, mc.cores=3,
@@ -195,47 +188,3 @@ fit <- sflist2stanfit(sflist)
 
 outfile <- paste("survival_stanfits_", sppList[do_species], ".RDS", sep="")
 saveRDS(fit, outfile)
-
-# fitted <- stan(fit=mcmc_samples, data=datalist, pars=pars,
-#                chains=3, iter=1000, warmup=250, init=inits)
-
-
-## Loop through and fit each species' model
-# for(do_species in sppList){
-#   survD <- subset(survD_all, species==do_species)
-#   
-#   clim_covs <- survD[,c("pptLag", "ppt1", "ppt2", "TmeanSpr1", "TmeanSpr2")]
-#   clim_covs$inter1 <- clim_covs$ppt1*clim_covs$TmeanSpr1
-#   clim_covs$inter2 <- clim_covs$ppt2*clim_covs$TmeanSpr2
-#   groups <- as.numeric(survD$Group)
-#   G <- length(unique(survD$Group))
-#   Yrs <- length(unique(survD$year))
-#   
-#   ## Set reasonable initial values for three chains
-#   inits <- list()
-#   inits[[1]] <- list(a_mu=0, a=rep(0,Yrs), b1_mu=0.01, b1=rep(0.01,Yrs),
-#                      gint=rep(0,G), w=0, sig_b1=0.5, sig_a=0.5,
-#                      sig_G=0.5, b2=rep(0,length(clim_covs)))
-#   inits[[2]] <- list(a_mu=1, a=rep(1,Yrs), b1_mu=1, b1=rep(1,Yrs),
-#                      gint=rep(1,G), w=0.5, sig_b1=1, sig_a=1,
-#                      sig_G=1, b2=rep(1,length(clim_covs)))
-#   inits[[3]] <- list(a_mu=0.5, a=rep(0.5,Yrs), b1_mu=0.5, b1=rep(0.5,Yrs),
-#                      gint=rep(-1,G), w=-0.5, sig_b1=0.1, sig_a=0.1,
-#                      sig_G=0.1, b2=rep(-1,length(clim_covs)))
-#   
-#   datalist <- list(N=nrow(survD), Yrs=Yrs, yid=(survD$year-31),
-#                    Covs=length(clim_covs), Y=survD$survives, X=log(survD$area),
-#                    C=clim_covs, W=survD$W, G=G, gid=groups)
-#   pars=c("a_mu", "a", "b1_mu",  "b1", "b2",
-#          "w", "gint")
-#   
-#   fitted <- stan(fit=mcmc_samples, data=datalist, pars=pars,
-#                  chains=3, iter=1000, warmup=250, init=inits)
-#   
-#   big_list[[do_species]] <- fitted
-# } # end species loop
-
-
-
-
-
