b55bfb2ccc245ada97ab9759a77fb864d4ea8e7a
Andrew Tredennick <atredenn@gmail.com>
2015-05-12 17:04:38 -0600

    adds parallel chain runs

--- montana/ipm/vitalRateRegs/growth/growthAllSpp_STAN.R
+++ montana/ipm/vitalRateRegs/growth/growthAllSpp_STAN.R
@@ -204,9 +204,15 @@ for(do_species in sppList){
                    C=clim_covs, W=W, G=G, gid=groups)
   pars=c("a_mu", "a", "b1_mu",  "b1", "b2",
          "w", "gint", "tau", "tauSize")
-  
-  fitted <- stan(fit=mcmc_samples, data=datalist, pars=pars,
-                 chains=3, iter=2000, warmup=1000, init=inits)
+  rng_seed <- 123
+  sflist <-
+    mclapply(1:3, mc.cores=3,
+             function(i) stan(fit=mcmc_samples, data=datalist, pars=pars,
+                              seed=rng_seed, chains=1, chain_id=i, refresh=-1,
+                              iter=2000, warmup=1000, init=list(inits[[i]])))
+  fit <- sflist2stanfit(sflist)
+#   fitted <- stan(fit=mcmc_samples, data=datalist, pars=pars,
+#                  chains=3, iter=2000, warmup=1000, init=inits)
   
   big_list[[do_species]] <- fitted
 } # end species loop
