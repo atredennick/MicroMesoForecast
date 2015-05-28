1bae4a57e2765488c849e578ecbfabc6a408dfdc
Andrew Tredennick <atredenn@gmail.com>
2015-05-28 14:33:36 -0600

    adds shell script for submitting all validation jobs

---
+++ montana/ipm/vitalRateRegs/validation/run_all_validation_regressions.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sbatch ./ipm_growth_validation/submit_growth_loyo.sh
+sbatch ./ipm_survival_validation/submit_survival_BOGR_loyo.sh
+sbatch ./ipm_survival_validation/submit_survival_HECO_loyo.sh
+sbatch ./ipm_survival_validation/submit_survival_PASM_loyo.sh
+sbatch ./ipm_survival_validation/submit_survival_POSE_loyo.sh
+sbatch ./ipm_rec_validation/submit_recruitment_loyo.sh
