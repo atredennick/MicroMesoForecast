94a39338538d321e1ae75523813c6703b41ece01
Andrew Tredennick <atredenn@gmail.com>
2015-06-15 11:02:37 -0600

    new version of QBM loyo simulation for oos one-step forecasts

--- analysis/quadBM/simulations/loyo_validation_qbm.R
+++ analysis/quadBM/simulations/loyo_validation_qbm.R
@@ -29,10 +29,10 @@
 rm(list=ls(all=TRUE))
 
 ## Set do_year for validation from command line prompt
-args <- commandArgs(trailingOnly = F)
-myargument <- args[length(args)]
-myargument <- sub("-","",myargument)
-do_year <- as.numeric(myargument)
+# args <- commandArgs(trailingOnly = F)
+# myargument <- args[length(args)]
+# myargument <- sub("-","",myargument)
+# do_year <- as.numeric(myargument)
 
 ##  Set number of simulations per year
 NumberSimsPerYear <- 100
@@ -51,115 +51,168 @@ library('EnvStats')
 ##  Observations
 allD <- read.csv("../../speciesData/quadAllCover.csv")
 allD <- allD[,2:ncol(allD)] #get rid of X ID column
-allD$percCover <- allD$totCover/10000
-head(scale(allD$percCover, center=TRUE, scale=TRUE))
 sppList <- as.character(unique(allD$Species))
 
-##  Climate
 climD <- read.csv("../../weather/Climate.csv")
 climD[2:6] <- scale(climD[2:6], center = TRUE, scale = TRUE)
 
+backD <- data.frame(climYear=NA,
+                    quad = NA,
+                    year= NA,
+                    totCover= NA,
+                    Species= NA,
+                    propCover= NA,
+                    lag.cover= NA,
+                    pptLag= NA,
+                    ppt1= NA,
+                    TmeanSpr1= NA,
+                    ppt2= NA,
+                    TmeanSpr2= NA,
+                    TmeanSum1= NA,
+                    TmeanSum2= NA,
+                    yearID= NA,
+                    group = NA,
+                    percCover = NA,
+                    percLagCover = NA)
 
-####
-####  Read in statistical model parameters ------------
-####
-in_file <- paste("../vitalRateRegressions/truncNormModel/validation/results/growthParamsMCMC_", do_year, ".rds", sep="")
-pGrow <- readRDS(in_file)
-pGrow2 <- melt(pGrow)
-pGrow2$Spp <- c(rep(rep(sppList, each=3000), times=12),
-                rep(rep(sppList, each=3000), times=1),
-                rep(rep(sppList, each=3000), times=6),
-                rep(rep(sppList, each=3000), times=12),
-                rep(rep(sppList, each=3000), times=7))
-pGrow2$Coef <- c(rep("beta", times=3000*4*12),
-                 rep("betaSpp", times=3000*4),
-                 rep("gInt", times=6*4*3000),
-                 rep("intYr", times=4*3000*12),
-                 rep("intercept", times=3000*4),
-                 rep("rain1", times=4*3000),
-                 rep("rain2", times=4*3000),
-                 rep("rainLag", times=4*3000),
-                 rep("tau", times=4*3000),
-                 rep("temp1", times=4*3000),
-                 rep("temp2", times=4*3000))
-colnames(pGrow2)[1] <- "Iter"
-pGrow <- pGrow2[,c(1,3:5)]; rm(pGrow2)
-pGrowAll <- subset(pGrow, Coef=="gInt"|Coef=="rain1"|
-                          Coef=="rain2"|Coef=="rainLag"|
-                          Coef=="temp1"|Coef=="temp2"|
-                          Coef=="tau"|Coef=="betaSpp"|Coef=="intercept")
+#loop through species and remake data frame
+for(spp in 1:length(sppList)){
+  doSpp <- sppList[spp]
+  sppD <- subset(allD, Species==doSpp)
+  
+  # create lag cover variable
+  tmp=sppD[,c("quad","year","totCover")]
+  tmp$year=tmp$year+1
+  names(tmp)[3]="lag.cover"
+  sppD=merge(sppD,tmp,all.x=T)
+  
+  # merge in climate data
+  sppD$climYear=sppD$year+1900-1  
+  sppD=merge(sppD,climD,by.x="climYear",by.y="year")
+  
+  #Growth observations
+  growD <- subset(sppD,lag.cover>0 & totCover>0)
+  growD$yearID <- growD$year #for random year offset on intercept
+  growD$group <- substring(growD$quad, 1, 1)
+  growD$percCover <- growD$totCover/10000
+  growD$percLagCover <- growD$lag.cover/10000
+  backD <- rbind(backD, growD)
+}#end species loop
+obs_data <- backD[2:nrow(backD),]
+all_years <- unique(obs_data$year)
 
 
 ####
 #### Population growth function ----------------------------
 ####
-growFunc <- function(pGrowAll, N, climate, 
-                     simsPerYear, sppSim, doGrp){
-  growNow <- subset(pGrowAll, Spp==sppSim)
-  doNow <- sample(x = c(1:3000), 1)
-  growNow <- subset(growNow, Iter==doNow)
-  iID <- which(growNow$Coef=="intercept")
-  intercept <- growNow$value[iID]
-  sID <- which(growNow$Coef=="betaSpp")
-  size <- growNow$value[sID]
-  cID <- which(growNow$Coef=="rainLag"|growNow$Coef=="rain1"|growNow$Coef=="rain2"|growNow$Coef=="temp1"|growNow$Coef=="temp2")
-  climEffs <- growNow$value[cID]
-  gID <- which(growNow$Coef=="intG")
-  intG <- growNow$value[gID][doGrp]
-  tID <- which(growNow$Coef=="tau")
-  tau <- growNow$value[tID]
-  mu <- intercept+size*log(N)+sum(climEffs*climate)
-  newN <- rlnormTrunc(1, meanlog = mu, sdlog = sqrt(1/tau), min = 0, max = 1)
+##  Define population growth function
+growFunc <- function(N, int, slope, clims, climcovs, tau){
+  mu <- int+slope*log(N)+sum(clims[1:7]*climcovs)+sum(clims[8:12]*log(N)*climcovs[1:5])
+  newN <- rlnormTrunc(1, meanlog = mu, sdlog = tau, min = 0, max = 1)
   return(newN)
 }
 
 
-####
-#### Run simulations -----------------------------------------------------
-####
-years <- as.numeric(unique(allD$year)+1900)
-yearsID <- unique(allD$year)
-yrD <- subset(allD, year==(do_year-1))
-climate <- subset(climD, year==(1900+do_year-1))[,c(2,3,5,4,6)]
-nSim <- NumberSimsPerYear
-
-outDraw <- data.frame(quad=NA, cover=NA, sim=NA, species=NA, year=NA)
-for(i in 1:length(sppList)){
-  sppSim <- sppList[i]
-  sppD <- subset(yrD, Species==sppSim)
-  quadList <- as.data.frame(as.character(unique(sppD$quad)))
-  quadList$group <- substring(quadList[,1], 1, 1)
-  quadList$groupNum <- as.numeric(as.factor(quadList$group))
-  colnames(quadList)[1] <- "quad"
-  
-  Nsave <- matrix(ncol=nrow(quadList), nrow=nSim)
-  for(qd in 1:nrow(quadList)){
-    Nstart <- subset(sppD, quad==as.character(quadList[qd,1]))$percCover
-    if(Nstart>0){
-      for(sim in 1:nSim){
-        Nout <- growFunc(pGrow=pGrowAll, N=Nstart, climate=climate, sppSim=sppSim, doGrp=quadList[qd,3])
-        Nsave[sim,qd] <- Nout
-        #       print(paste("simulation", sim, "of year", yr, "in quad", quadList[qd,1], "for", sppList[i]))
-      }#end simulations loop
-    }#End empty quad if statement
-  }#end group loop
-  dN <- as.data.frame(Nsave)
-  colnames(dN) <- as.character(quadList[,1])
-  nM <- melt(dN)
-  nM$sim <- rep(1:nSim, nrow(quadList))
-  nM$species <- rep(sppSim, nSim*nrow(quadList))
-  nM$year <- do_year
-  colnames(nM)[1:2] <- c("quad", "cover")
-  outDraw <- rbind(outDraw, nM)
+##  Start looping over species and year within species
+outall <- data.frame(quad=NA, sim=NA, species=NA, 
+                     year=NA, cover.t1=NA, cover.t0=NA)
+for(do_species in sppList){
+  for(do_year in all_years){
+    ####
+    ####  Read in statistical model parameters ------------
+    ####
+    do_year <- 1
+    yearnow <- all_years[do_year]
+    do_species <- "BOGR"
+    
+    
+    fitlong <- readRDS(paste("../vitalRateRegressions/truncNormModel/validation/fits/popgrowth_stanmcmc_", 
+                             do_species, "_leaveout", yearnow, ".RDS", sep=""))
+    ##  Break up MCMC into regression components
+    fitthin <- fitlong
+    # Climate effects
+    climeff <- fitthin[grep("b2", fitthin$Parameter),]
+    
+    # Mean cover (size) effects
+    coveff <- fitthin[grep(glob2rx("b1_mu"), fitthin$Parameter),]
+    
+    # Mean intercepts
+    intercept <- fitthin[grep("a_mu", fitthin$Parameter),]
+    
+    # Group offsets
+    goffs <- fitthin[grep("gint", fitthin$Parameter),]
+    goffs$groupid <- substr(goffs$Parameter, 6, length(goffs$Parameter))
+    goffs$groupid <- unlist(strsplit(goffs$groupid, split=']'))
+    
+    # Lognormal sigma (called tau here)
+    tau <- fitthin[grep("tau", fitthin$Parameter),]
+    
+    
+    ####
+    #### Run simulations -----------------------------------------------------
+    ####
+    yrD <- subset(obs_data, year==yearnow & Species==do_species)
+    
+    # Get first row of data for climate since climate is the same within years
+    climcovs <- yrD[1,c("pptLag", "ppt1", "ppt2", "TmeanSpr1", "TmeanSpr2")]
+    climcovs$inter1 <- climcovs$ppt1*climcovs$TmeanSpr1
+    climcovs$inter2 <- climcovs$ppt2*climcovs$TmeanSpr2
+    
+    nSim <- NumberSimsPerYear
+    
+    quadList <- as.data.frame(as.character(unique(yrD$quad)))
+    quadList$group <- substring(quadList[,1], 1, 1)
+    quadList$groupNum <- as.numeric(as.factor(quadList$group))
+    colnames(quadList)[1] <- "quad"
+    Nsave <- matrix(ncol=nrow(quadList), nrow=nSim)
+    Nstarts <- matrix(ncol=nrow(quadList), nrow=nSim)
+    # Loop over quads to make one-step forecast nSim times
+    for(qd in 1:nrow(quadList)){
+      Nstart <- subset(yrD, quad==as.character(quadList[qd,1]))$percLagCover
+      if(Nstart>0){
+        for(sim in 1:nSim){
+          randchain <- sample(x = climeff$Chain, size = 1)
+          randiter <- sample(x = climeff$Iteration, size = 1)
+          inttmp <- subset(intercept, Chain==randchain & 
+                             Iteration==randiter)
+          grptmp <- subset(goffs, Chain==randchain & 
+                             Iteration==randiter &
+                             groupid==quadList[qd,"groupNum"])
+          slopetmp <- subset(coveff, Chain==randchain & 
+                               Iteration==randiter)
+          tmpclim <- subset(climeff, Chain==randchain & 
+                              Iteration==randiter)
+          tmptau <- subset(tau, Chain==randchain & 
+                             Iteration==randiter)
+          Nout <- growFunc(N = Nstart, int = inttmp$value+grptmp$value, 
+                           slope = slopetmp$value, clims = tmpclim$value,
+                           climcovs = climcovs, tau = tmptau$value) 
+          Nsave[sim,qd] <- Nout
+          Nstarts[sim,qd] <- Nstart
+          print(paste("simulation", sim, "of year", do_year, "in quad", quadList[qd,1], "for", do_species))
+        }#end simulations loop
+      }#End empty quad if statement
+    }#end group loop
+    newNs <- as.data.frame(Nsave)
+    colnames(newNs) <- as.character(quadList[,1])
+    newNs <- melt(newNs)
+    newNs$sim <- rep(1:nSim, nrow(quadList))
+    newNs$species <- rep(do_species, nSim*nrow(quadList))
+    newNs$year <- yearnow
+    colnames(newNs)[1:2] <- c("quad", "cover.t1")
+    
+    startNs <- as.data.frame(Nstarts)
+    colnames(startNs) <- as.character(quadList[,1])
+    startNs <- melt(startNs)
+    startNs$sim <- rep(1:nSim, nrow(quadList))
+    startNs$species <- rep(do_species, nSim*nrow(quadList))
+    startNs$year <- yearnow
+    colnames(startNs)[1:2] <- c("quad", "cover.t0")
+    
+    outsaves <- merge(newNs, startNs)
+  }#end year loop
+  outall <- rbind(outall, outsaves)
 }#end species loop
 
-sims_to_remove <- which(is.na(outDraw$cover)==TRUE)
-loyo_simulations <- outDraw[-sims_to_remove,]
-colnames(loyo_simulations) <- c("quad", "predicted_cover",
-                                "sim", "species", "predicted_year")
 
-####
-####  Save output ---------------------------------
-####
-out_file <- paste("loyo_validation_sims_", do_year, ".rds", sep="")
-saveRDS(loyo_simulations, out_file)
+
