db2652305a18f7776f1d01178d776802b8bb33f9
Andrew Tredennick <atredenn@gmail.com>
2016-03-28 10:00:11 -0600

    updates qbm with correct covariate structure and priuors

--- analysis/quadBM/vitalRateRegressions/truncNormModel/popgrowth_STAN.R
+++ analysis/quadBM/vitalRateRegressions/truncNormModel/popgrowth_STAN.R
@@ -1,8 +1,13 @@
 ## Script to estimate parameters for the quad-based model using STAN
 
+rm(list=ls())
+
 library(rstan)
 library(parallel)
 library(ggmcmc)
+
+priors_df <- read.csv("../../../all_maxlppds.csv")
+priors <- subset(priors_df, vital=="cover")
 
 ##  STAN model
 model_string <- "
@@ -16,6 +21,7 @@ data{
   real<lower=0,upper=1> Y[N]; // observation vector
   matrix[N,Covs] C; // climate matrix
   vector[N] X; // size vector
+  real sdclim; // prior for climate effects
 }
 parameters{
   real a_mu;
@@ -44,7 +50,7 @@ model{
   sig_b1 ~ cauchy(0,5);
   sig_G ~ cauchy(0,5);
   gint ~ normal(0, sig_G);
-  b2 ~ normal(0,0.1);
+  b2 ~ normal(0,sdclim);
   a ~ normal(a_mu, sig_a);
   b1 ~ normal(b1_mu, sig_b1);
   tau ~ cauchy(0,5);
@@ -61,7 +67,7 @@ allD <- allD[,2:ncol(allD)] #get rid of X ID column
 sppList <- as.character(unique(allD$Species))
 
 climD <- read.csv("../../../weather/Climate.csv")
-climD[2:6] <- scale(climD[2:6], center = TRUE, scale = TRUE)
+clim_vars <- c("pptLag", "ppt1", "ppt2", "TmeanSpr1", "TmeanSpr2")
 
 backD <- data.frame(climYear=NA,
                     quad = NA,
@@ -109,21 +115,29 @@ growD_all <- backD[2:nrow(backD),]
 
 
 growD <- subset(growD_all, Species=="BOGR")
-clim_covs <- growD[,c("pptLag", "ppt1", "ppt2", "TmeanSpr1", "TmeanSpr2")]
-clim_covs$inter1 <- clim_covs$ppt1*clim_covs$TmeanSpr1
-clim_covs$inter2 <- clim_covs$ppt2*clim_covs$TmeanSpr2
-clim_covs$sizepptLag <- clim_covs$pptLag*log(growD$percLagCover)
-clim_covs$sizeppt1 <- clim_covs$ppt1*log(growD$percLagCover)
-clim_covs$sizeppt2 <- clim_covs$ppt2*log(growD$percLagCover)
-clim_covs$sizetemp1 <- clim_covs$TmeanSpr1*log(growD$percLagCover)
-clim_covs$sizetemp2 <- clim_covs$TmeanSpr2*log(growD$percLagCover)
+##  Create and scale interaction covariates
+growD$ppt1TmeanSpr1 <- growD$ppt1*growD$TmeanSpr1
+growD$ppt2TmeanSpr2 <- growD$ppt2*growD$TmeanSpr2
+growD$sizepptLag <- growD$pptLag*log(growD$percLagCover)
+growD$sizeppt1 <- growD$ppt1*log(growD$percLagCover)
+growD$sizeppt2 <- growD$ppt2*log(growD$percLagCover)
+growD$sizeTmeanSpr1 <- growD$TmeanSpr1*log(growD$percLagCover)
+growD$sizeTmeanSpr2 <- growD$TmeanSpr2*log(growD$percLagCover)
+clim_vars_all <- c(clim_vars, "ppt1TmeanSpr1", "ppt2TmeanSpr2", "sizepptLag",
+                   "sizeppt1", "sizeppt2", "sizeTmeanSpr1", "sizeTmeanSpr2")
+clim_covs <- growD[,clim_vars_all]
+# Get scalers for climate covariates from training data
+clim_means <- colMeans(clim_covs)
+clim_sds <- apply(clim_covs, 2, FUN = sd)
+clim_covs <- scale(clim_covs, center = TRUE, scale = TRUE)
 groups <- as.numeric(as.factor(growD$group))
 G <- length(unique(growD$group))
 Yrs <- length(unique(growD$year))
+yid <- as.numeric(as.factor(growD$year))
 
-datalist <- list(N=nrow(growD), Yrs=Yrs, yid=(growD$year-32),
-                 Covs=length(clim_covs), Y=growD$percCover, X=log(growD$percLagCover),
-                 C=clim_covs, G=G, gid=groups)
+datalist <- list(N=nrow(growD), Yrs=Yrs, yid=yid,
+                 Covs=ncol(clim_covs), Y=growD$percCover, X=log(growD$percLagCover),
+                 C=clim_covs, G=G, gid=groups, sdclim=0.1)
 pars=c("a_mu", "a", "b1_mu",  "b1", "b2",
        "tau", "gint")
 
@@ -131,20 +145,31 @@ mcmc_samples <- stan(model_code=model_string, data=datalist,
                      pars=pars, chains=0)
 
 ##  Loop through species and fit the model
-big_list <- list()
 for (do_species in sppList){
+  print(paste("fitting model for", do_species))
+  
   growD <- subset(growD_all, Species==do_species)
-  clim_covs <- growD[,c("pptLag", "ppt1", "ppt2", "TmeanSpr1", "TmeanSpr2")]
-  clim_covs$inter1 <- clim_covs$ppt1*clim_covs$TmeanSpr1
-  clim_covs$inter2 <- clim_covs$ppt2*clim_covs$TmeanSpr2
-  clim_covs$sizepptLag <- clim_covs$pptLag*log(growD$percLagCover)
-  clim_covs$sizeppt1 <- clim_covs$ppt1*log(growD$percLagCover)
-  clim_covs$sizeppt2 <- clim_covs$ppt2*log(growD$percLagCover)
-  clim_covs$sizetemp1 <- clim_covs$TmeanSpr1*log(growD$percLagCover)
-  clim_covs$sizetemp2 <- clim_covs$TmeanSpr2*log(growD$percLagCover)
+  prior_stddev <- as.numeric(subset(priors, species==do_species)["prior_stdev"])
+  
+  ##  Create and scale interaction covariates
+  growD$ppt1TmeanSpr1 <- growD$ppt1*growD$TmeanSpr1
+  growD$ppt2TmeanSpr2 <- growD$ppt2*growD$TmeanSpr2
+  growD$sizepptLag <- growD$pptLag*log(growD$percLagCover)
+  growD$sizeppt1 <- growD$ppt1*log(growD$percLagCover)
+  growD$sizeppt2 <- growD$ppt2*log(growD$percLagCover)
+  growD$sizeTmeanSpr1 <- growD$TmeanSpr1*log(growD$percLagCover)
+  growD$sizeTmeanSpr2 <- growD$TmeanSpr2*log(growD$percLagCover)
+  clim_vars_all <- c(clim_vars, "ppt1TmeanSpr1", "ppt2TmeanSpr2", "sizepptLag",
+                     "sizeppt1", "sizeppt2", "sizeTmeanSpr1", "sizeTmeanSpr2")
+  clim_covs <- growD[,clim_vars_all]
+  # Get scalers for climate covariates from training data
+  clim_means <- colMeans(clim_covs)
+  clim_sds <- apply(clim_covs, 2, FUN = sd)
+  clim_covs <- scale(clim_covs, center = TRUE, scale = TRUE)
   groups <- as.numeric(as.factor(growD$group))
   G <- length(unique(growD$group))
   Yrs <- length(unique(growD$year))
+  yid <- as.numeric(as.factor(growD$year))
   
   ## Set reasonable initial values for three chains
   inits <- list()
@@ -158,9 +183,9 @@ for (do_species in sppList){
                      gint=rep(0.5,G), w=c(-0.5,-0.5), sig_b1=0.1, sig_a=0.1, tau=0.1, tauSize=0.1,
                      sig_G=0.1, b2=rep(-1,length(clim_covs)))
   
-  datalist <- list(N=nrow(growD), Yrs=Yrs, yid=(growD$year-32),
+  datalist <- list(N=nrow(growD), Yrs=Yrs, yid=yid,
                    Covs=length(clim_covs), Y=growD$percCover, X=log(growD$percLagCover),
-                   C=clim_covs, G=G, gid=groups)
+                   C=clim_covs, G=G, gid=groups, sdclim=prior_stddev)
   pars=c("a_mu", "a", "b1_mu",  "b1", "b2",
          "tau", "gint")
   
@@ -173,12 +198,5 @@ for (do_species in sppList){
   fit <- sflist2stanfit(sflist)
   long <- ggs(fit)
   saveRDS(long, paste("popgrowth_stanmcmc_", do_species, ".RDS", sep=""))
-#   big_list[[do_species]] <- fit
 }
 
-# saveRDS(big_list, "popgrowth_stanfits.RDS")
-
-
-# fitted <- stan(fit=mcmc_samples, data=datalist, pars=pars,
-#                chains=1, iter=200, warmup=100, init=list(inits[[3]]))
-# traceplot(fitted)
