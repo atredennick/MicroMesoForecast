93d35efa7070e8227770805c10802af86fecd2a0
Andrew Tredennick <atredenn@gmail.com>
2016-04-06 18:57:14 -0600

    collapses climate change IPM scripts with new functions

--- analysis/ipm/simulations/climate_change_wrapper.R
+++ analysis/ipm/simulations/climate_change_wrapper.R
@@ -1,105 +1,211 @@
-###############################################################
-#### Wrapper for running climate change scenarios by species
+#### Wrapper for running IPM climate change scenarios by species
 ####
 #### Andrew Tredennick
 #### 2-11-2014
+#### Updated: 4-6-2016
+
+
+do_change <- 1 # Determines the level of climate change programmatically
+
+
 ####
+####  PRELIMINARIES
+####
+##  Use mean parameter values?
+use_mean_params <- TRUE
 
-root=ifelse(.Platform$OS.type=="windows","c:/repos","~/repos"); # modify as needed
-setwd(paste(root,"/MicroMesoForecast/analysis/ipm/simulations",sep="")); # modify as needed 
+##  Set working directory programmatically
+root=ifelse(.Platform$OS.type=="windows","c:/repos","~/repos") # modify as needed
+setwd(paste(root,"/MicroMesoForecast/analysis/ipm/simulations",sep="")) # modify as needed 
 
-# Set some global parameters for the simulations
-tlimit<-2500  # number of years to simulate
-burn.in<-500    # years to cut before calculations
+##  Directory paths
+if(use_mean_params==TRUE){
+  resultspath <- "./results/climchange_meanparams/"
+}
+if(use_mean_params==FALSE){
+  resultspath <- "./results/climchange_varyparams/"
+}
+
+##  Global parameters for the simulations
+tlimit <- 250  # number of years to simulate
+burn.in <- 50    # years to cut before calculations
 spp_list <- c("BOGR","HECO","PASM","POSE")
 n_spp <- length(spp_list)
-clim_change <- c(0.01, 0.1, 0.2, 0.3) # climate change percentage
-perc_change <- clim_change*100
 
+##  Degree of climate change (can be a vector)
+# clim_change <- c(0.01, 0.1, 0.2, 0.3) # climate change percentage
+clim_change <- 0.01 # climate change percentage in decimal form
+perc_change <- clim_change*100 # climate change percentage in %age form
+clim_alt <- clim_change[do_change] # can be set programmatically if necessary
+filetag <- paste(perc_change[do_change], ".csv", sep="")
 
-# Get climate and random year effect sequences
+##  Load climate scalers
+growth_clim_scalers <- readRDS("../../growth_all_clim_scalers.RDS")
+surv_clim_scalers <- readRDS("../../survival_all_clim_scalers.RDS")
+rec_clim_scalers <- readRDS("../../recruitment_all_clim_scalers.RDS")
+
+## Load climate and random year effect sequences
 yrSave <- readRDS("../../random_year_effects_sequence.rds")
 climYr <- readRDS("../../climate_year_sequence.rds")
-# paramSeq <- readRDS("../../random_chain_iter_sequence.RDS")
 
-# get calendar years
+## Fetch calendar years
 raw_data <- read.csv("../../speciesData/quadAllCover.csv")
 years <- unique(raw_data$year)
 years <- years[1:(length(years)-1)] #lop off 1945 since no climate for that year
 
-# get alphas values (needed to calculate neighborhood crowding)
+##  Fetch alpha values (needed to calculate neighborhood crowding)
+##  Alpha values come directly from Chu and Adler (2015, Ecological Monographs)
 alpha_grow <- read.csv("../../alpha_list_growth.csv")
 alpha_surv <- read.csv("../../alpha_list_survival.csv")
 
-# Source functions
-source("vital_rate_ipm_functions.R", echo = FALSE)
-source("../vitalRateRegs/survival/import2ipm.R", echo = FALSE)
-source("../vitalRateRegs/growth/import2ipm.R", echo = FALSE)
-source("../vitalRateRegs/recruitment/import2ipm.R", echo = FALSE)
 
 
-# ####
-# #### Simulation 1. Observed climate sequence -------------------------------------
-# ####
-# # Set climate for observed climate run
-# clim_data <- read.csv("../../weather/Climate.csv")
-# clim_data <- clim_data[,c("year", "pptLag","ppt1","ppt2","TmeanSpr1","TmeanSpr2")] # subset and reorder to match regression param import
-# clim_data[2:6] <- scale(clim_data[2:6], center = TRUE, scale = TRUE) # standardize
-# 
-# # Loop through species
-# # n_spp <- 1 #for test
-# for(ss in 1:n_spp){
-#   doSpp <- spp_list[ss]
-#   outfile1<-paste("./results/climchange_varyparams/",doSpp,"_ipm_cover_obsClimate.csv",sep="")
-#   outfile2<-paste("./results/climchange_varyparams/",doSpp,"_ipm_density_obsClimate.csv",sep="")
-#   outfile3<-paste("./results/climchange_varyparams/",doSpp,"_ipm_stableSize_obsClimate.csv",sep="")
-#   climtag <- "obs"
-#   source("ipm_climate_simulations.R", echo = FALSE)
-#   print(paste("DONE WITH", doSpp, "FOR OBSERVED CLIMATE SIMULATION"))
-# }
+####
+####  SOURCE FUNCTION FILES FOR VITAL RATES AND PARAMETER IMPORT
+####
+source("vital_rate_ipm_functions.R", echo = FALSE) # for IPM
+
+##  Functions for fetching vital rate parameters
+if(use_mean_params==TRUE){
+  source("../vitalRateRegs/survival/import2ipm_means.R", echo = FALSE)
+  source("../vitalRateRegs/growth/import2ipm_means.R", echo = FALSE)
+  source("../vitalRateRegs/recruitment/import2ipm_means.R", echo = FALSE)
+}
+if(use_mean_params==FALSE){
+  source("../vitalRateRegs/survival/import2ipm.R", echo = FALSE)
+  source("../vitalRateRegs/growth/import2ipm.R", echo = FALSE)
+  source("../vitalRateRegs/recruitment/import2ipm.R", echo = FALSE) 
+}
 
 
-for(ii in 3:length(clim_change)){
-  clim_alt <- clim_change[ii]
-  filetag <- paste(perc_change[ii], ".csv", sep="")
+
+####
+####  LOAD CLIMATE DATA (UNSTANDARDIZED)
+####
+clim_data_obs <- read.csv("../../weather/Climate.csv")
+# Subset and reorder to match regression param import
+clim_data_obs <- clim_data_obs[,c("year", "pptLag","ppt1","ppt2","TmeanSpr1","TmeanSpr2")]
+
+
+
+####
+####  FUNCTION FOR CLIMATE CHANGE IPM SIMULATIONS
+####
+climsimIPM <- function(clim_alt, clim_var, clim_data_obs, do_spp){
+  doSpp <- spp_list[do_spp]
   
-  ####
-  #### Simulation 2. 1% increase in precipitation -------------------------------------
-  ####
-  # Set climate for 1% ppt increase run
-  clim_data <- read.csv("../../weather/Climate.csv")
-  clim_data <- clim_data[,c("year", "pptLag","ppt1","ppt2","TmeanSpr1","TmeanSpr2")] # subset and reorder to match regression param import
-  # clim_data[2:5] <- scale(clim_data[2:5], center = TRUE, scale = TRUE) # standardize
-  clim_avg <- apply(X = clim_data, MARGIN = 2, FUN = mean)
-  clim_sd <- apply(X = clim_data, MARGIN = 2, FUN = sd)
-  
-  # Get just the variables of interest
-  vars <- grep("ppt",names(clim_data))
-  tmp1 <- clim_alt*colMeans(clim_data)
-  tmp1 <- matrix(tmp1,NROW(clim_data),NCOL(clim_data),byrow=T)
-  clim_data[,vars]=clim_data[,vars]+tmp1[,vars]
-  
-  # Now scale based on perturbed or regular data, depending on scenario
-  clim_data["pptLag"] <- (clim_data["pptLag"] - clim_avg["pptLag"])/clim_sd["pptLag"]
-  clim_data["ppt1"] <- (clim_data["ppt1"] - clim_avg["ppt1"])/clim_sd["ppt1"]
-  clim_data["ppt2"] <- (clim_data["ppt2"] - clim_avg["ppt2"])/clim_sd["ppt2"]
-  clim_data["TmeanSpr1"] <- (clim_data["TmeanSpr1"] - clim_avg["TmeanSpr1"])/clim_sd["TmeanSpr1"]
-  clim_data["TmeanSpr2"] <- (clim_data["TmeanSpr2"] - clim_avg["TmeanSpr2"])/clim_sd["TmeanSpr2"]
-  
-  # Loop through species
-  spp_list <- c("BOGR","HECO","PASM","POSE")
-  n_spp <- length(spp_list)
-  # n_spp <- 1
-  for(ss in 1:n_spp){
-    doSpp <- spp_list[ss]
-    outfile1<-paste("./results/climchange_varyparams/",doSpp,"_ipm_cover_pptChange", filetag ,sep="")
-    outfile2<-paste("./results/climchange_varyparams/",doSpp,"_ipm_density_pptChange", filetag,sep="")
-    outfile3<-paste("./results/climchange_varyparams/",doSpp,"_ipm_stableSize_pptChange", filetag,sep="")
-    climtag <- "ppt"
-    source("ipm_climate_simulations.R", echo = FALSE)
-    print(paste("DONE WITH", doSpp, "FOR PRECIPITATION CHANGE SIMULATION"))
+  if(is.na(clim_var)==TRUE){
+    clim_data <- clim_data_obs
   }
   
+  if(is.na(clim_var)==FALSE){
+    # Perturb climate time series
+    clim_data <- clim_data_obs # re-assigns the temporary clim_data object
+    for(ivar in 1:length(clim_var)){
+      vars <- grep(clim_var[ivar],names(clim_data_obs)) # indices for ppt columns
+      tmp1 <- clim_alt*colMeans(clim_data_obs) # vector of ppt additions
+      # matrix of additions to match DIMS of observed climate
+      tmp1 <- matrix(tmp1,NROW(clim_data_obs),NCOL(clim_data_obs),byrow=T) 
+      if(colMeans(tmp1)[1]!=tmp1[1,1]) { stop("climate change matrix mis-aligned") }
+      clim_data[,vars] <- clim_data_obs[,vars]+tmp1[,vars] # applies ppt additions
+    }
+    # Make sure climate perturbations have been applied
+    if(mean(clim_data$pptLag)==mean(clim_data_obs$pptLag)) {
+      stop("climate perturbation not applied")
+    }
+  }
+  
+  # Retriev spp-specific scalers
+  growth_clim_scalers <- subset(growth_clim_scalers, yearout==1 & species==doSpp)
+  surv_clim_scalers <- subset(surv_clim_scalers, yearout==1 & species==doSpp)
+  rec_clim_scalers <- subset(rec_clim_scalers, yearout==1 & species==doSpp)
+  Gscalers <- growth_clim_scalers[,c("means","sds")]
+  Sscalers <- surv_clim_scalers[,c("means","sds")]
+  Rscalers <- rec_clim_scalers[,c("means","sds")]
+  
+  # Run the IPM
+  source("ipm_climate_simulations.R", echo = FALSE)
+  
+  # Throw error if something weird happens
+  if(min(covSave)<0.00001) { stop("really low values; check IPM") }
+  if(max(covSave)>1) { stop("cover greater than 1; check IPM") }
+  
+  return(covSave)
+}
+
+
+####
+#### RUN CLIMATE CHANGE SIMULATIONS
+####
+## Simulation 1: Observed Climate
+clim_var <- NA
+for(ispp in 1:length(spp_list)){
+  covSave <- climsimIPM(clim_alt, clim_var, clim_data_obs, do_spp = ispp)
+  
+  # Set output filename
+  filenameid <- paste(clim_var, collapse="")
+  outfile <- paste0(resultspath,doSpp,"_ipm_cover_", filenameid, "Change.csv")
+  climtag <- clim_var # tag for current climate simulations
+  
+  # Save cover time series (covSave from IPM)
+  output <- data.frame("time"=burn.in:tlimit,"cover"=covSave[burn.in:tlimit])
+  output$species <- doSpp
+  output$climsim <- filenameid
+  # write.table(output,outfile,row.names=F,sep=",")
+  cat(paste("DONE WITH", spp_list[ispp], "FOR OBSERVED CLIMATE SIMULATION"))
+  cat("\n") # makes a space before next line printing
+}
+
+
+####
+####  SIMULATION 2: INCREASE IN PRECIPITATION
+####
+# Perturb climate time series
+vars <- grep("ppt",names(clim_data_obs)) # indices for ppt columns
+tmp1 <- clim_alt*colMeans(clim_data_obs) # vector of ppt additions
+
+# matrix of additions to match DIMS of observed climate
+tmp1 <- matrix(tmp1,NROW(clim_data_obs),NCOL(clim_data_obs),byrow=F) 
+
+clim_data <- clim_data_obs # re-assigns the temporary clim_data object
+clim_data[,vars] <- clim_data_obs[,vars]+tmp1[,vars] # applies ppt additions
+
+for(ss in 1:n_spp){
+  doSpp <- spp_list[ss] # set the species
+  
+  # Retriev spp-specific scalers
+  growth_clim_scalers <- subset(growth_clim_scalers, yearout==1 & species==doSpp)
+  surv_clim_scalers <- subset(surv_clim_scalers, yearout==1 & species==doSpp)
+  rec_clim_scalers <- subset(rec_clim_scalers, yearout==1 & species==doSpp)
+  Gscalers <- growth_clim_scalers[,c("means","sds")]
+  Sscalers <- surv_clim_scalers[,c("means","sds")]
+  Rscalers <- rec_clim_scalers[,c("means","sds")]
+  
+  # Set output filenames
+  outfile <- paste0(resultspath,doSpp,"_ipm_cover_pptChange.csv")
+  climtag <- "ppt" # tag for current climate simulations
+  
+  # Make sure climate perturbations have been applied
+  if(mean(clim_data$pptLag)==mean(clim_data_obs$pptLag)) {
+    stop("climate perturbation not applied")
+  }
+  
+  # Run the IPM
+  source("ipm_climate_simulations.R", echo = FALSE)
+  
+  # Throw error if something weird happens
+  if(min(covSave)<0.00001) { stop("really low values; check IPM") }
+  if(max(covSave)>1) { stop("cover greater than 1; check IPM") }
+  
+  # Save cover time series (covSave from IPM)
+  output <- data.frame("time"=burn.in:tlimit,"cover"=covSave[burn.in:tlimit])
+  output$species <- doSpp
+  output$climsim <- climtag
+  write.table(output,outfile,row.names=F,sep=",")
+  cat(paste("DONE WITH", doSpp, "FOR INCREASED PRECIPITATION SIMULATION"))
+  cat("\n") # makes a space before next line printing
+}
+
   
   
   ####
--- analysis/ipm/simulations/ipm_climate_simulations.R
+++ analysis/ipm/simulations/ipm_climate_simulations.R
@@ -1,191 +1,188 @@
+##  Single species IPM with climate covariates
+##  This script simulates equilibrium cover
 
-# single species IPM with climate covariates
-# this script simulates equilibrium cover
 
-#============================================================
-# (I) INPUTS
-#============================================================
-doGroup=NA  # NA for spatial avg., values 1-6 for a specific group
-initialCover<-c(0.01)
-# nMCMC<-3000 # max number of MCMC iterations to draw parameters from
 
-#============================================================
-# (II) LOAD VITAL RATE FUNCTIONS & SET UP PARAMETERS
-#============================================================
-
-sppCode<-which(spp_list==doSpp)
-# n_spp<-length(spp_list) # this is needed b/c all 4 spp parameters are imported at once
-
-# get recruit size parameters
-rec_size_mean <- numeric(n_spp)
-rec_size_var <- numeric(n_spp)
-for(i in 1:n_spp){
-  infile=paste("../../speciesData/",spp_list[i],"/recSize.csv",sep="")
-  recSize=read.csv(infile)
-  rec_size_mean[i]=mean(log(recSize$area))
-  rec_size_var[i]=var(log(recSize$area))
-}
-
-# pull out alphas only for the doSpp
-alphaG <- subset(alpha_grow, Site=="Montana")$Alpha[sppCode]
-alphaS <- subset(alpha_surv, Site=="Montana")$Alpha[sppCode]
-
-#============================================================================================#
-# (II) Simulation length, Matrix size and initial vectors
-#============================================================================================#
-
-Atotal=10000 #Area of 100cm x 100cm quadrat
-
-bigM=c(75,50,10,50) [sppCode]            #Set matrix dimension for each species
-maxSize=c(2500,120,25,100) [sppCode]    
-
-v=v.r=b.r=expv=Cr=WmatG=WmatS=NULL #a LIST for different species
-h=r.L=r.U=Ctot=NULL
-
-# stuff for numerical approximation....
-  # minimum (0.9*minimum size from data) and maximum sizes (1.1*maximum size from data)
-  # log-transformation...
-  # then L, U, b, v, h are all log-transformed....
-L=log(0.2)
-U=log(maxSize)*1.1     
-  
-# b---boundary points. Note: b chops up the size interval (L-U) into bigM-equal-sized portions.
-b = L+c(0:bigM)*(U-L)/bigM #log-transformed
-  
-# v---middle points (in Ellner's script, v is y); calculates the middle of each n-equal-sized portion.
-v = 0.5*(b[1:bigM]+b[2:(bigM+1)]) #log-transformed
- 
-# step size for midpoint rule. (see equations 4 and 5 in Ellner and Rees (2006) Am Nat.)
-h = v[2]-v[1] #log-transoformed 
-
-# variables for Wr approximation---radius
-b.r=sqrt(exp(b)/pi) 
-v.r=sqrt(exp(v)/pi)
-expv=exp(v) #for size
-r.L=sqrt(exp(L)/pi) #the lower size limit; radius
-r.U=sqrt(exp(U)/pi) #the upper size limit; radius
-WmatG=rep(NA,length(v.r))  # storage of size-specific conspecific W values for each species
-WmatS=rep(NA,length(v.r))  # storage of size-specific conspecific W values for each species
-
-tmp=range(v.r)#this is the 'real" range, not log-transformed...
-size.range=seq(tmp[1],tmp[2],length=bigM) # range across all possible sizes; 'real' radius for each size stage
-
-#============================================================================================#
-# (III) Utility functions
-#============================================================================================#
-
-#Using u and v for size variables instead of x and y, so that x and y can be used for locations in space
-#v above is also for the size (middle point size---area)
-# load the necessary libraries
+####
+####  LOAD LIBRARIES
+####
 library(boot)
 library(mvtnorm)
 library(msm)
 library(statmod) #"Statistical Modeling" 
 
-##### Put all component matrices into 3-dimensional arrays 
+####
+####  INPUTS
+####
+doGroup <- NA  # NA for spatial avg., values 1-6 for a specific group
+initialCover <- c(0.01) # arbitrarily low starting cover
 
-# combined kernel
-make.K.values=function(v,u,muWG,muWS, #state variables
-                       Rpars,rpa,Gpars,Spars,doYear,doSpp,weather)  #vital rate arguments
+
+
+####
+####  SET UP PARAMETERS
+####
+sppCode <- which(spp_list==doSpp) # gets numeric code for current species (1:4)
+
+##  Fetch recruit size parameters
+rec_size_mean <- numeric(n_spp)
+rec_size_var <- numeric(n_spp)
+for(i in 1:n_spp){
+  infile=paste("../../speciesData/",spp_list[i],"/recSize.csv",sep="")
+  recSize=read.csv(infile)
+  rec_size_mean[i]=mean(log(recSize$area)) # mean new recuit size
+  rec_size_var[i]=var(log(recSize$area)) # variance in new recruitm size
+}
+
+##  Fetch alpha current species
+alphaG <- subset(alpha_grow, Site=="Montana")$Alpha[sppCode]
+alphaS <- subset(alpha_surv, Site=="Montana")$Alpha[sppCode]
+
+
+
+####
+#### SIMULATION LENGTH, MATRIX SIZE, AND INITIAL VECTORS
+####
+Atotal <- 10000 # Area of 100cm x 100cm quadrat
+bigM <- c(75,50,10,50) [sppCode] # Get matrix dimension for current species
+maxSize <- c(2500,120,25,100) [sppCode] # Get maximum size for current species    
+
+##  Initialize object names, set to NULL
+v <- v.r <- b.r <- expv <- Cr <- WmatG <- WmatS <- NULL 
+h <- r.L <- r.U <- Ctot <- NULL
+
+# stuff for numerical approximation....
+# minimum (0.2) and maximum sizes (1.1*maximum size from data)
+# log-transformation...
+# then L, U, b, v, h are all log-transformed....
+L <- log(0.2)
+U <- log(maxSize)*1.1     
+  
+# b: boundary points. 
+# Note: b chops up the size interval (L-U) into bigM-equal-sized portions.
+b <- L+c(0:bigM)*(U-L)/bigM # log-transformed
+  
+# v: middle points (in Ellner's script, v is y); 
+# calculates the middle of each n-equal-sized portion.
+v <- 0.5*(b[1:bigM]+b[2:(bigM+1)]) # log-transformed
+ 
+# step size for midpoint rule. 
+# see equations 4 and 5 in Ellner and Rees (2006) Am Nat.
+h <- v[2]-v[1] # log-transoformed 
+
+# variables for Wr approximation: radius (has to do with crowding)
+b.r <- sqrt(exp(b)/pi) 
+v.r <- sqrt(exp(v)/pi)
+expv <- exp(v) # for size
+r.L <- sqrt(exp(L)/pi) # the lower size limit; radius
+r.U <- sqrt(exp(U)/pi) # the upper size limit; radius
+
+# storage of size-specific conspecific W values for growth for each species
+WmatG <- rep(NA,length(v.r))
+
+# storage of size-specific conspecific W values for survival for each species
+WmatS <- rep(NA,length(v.r))
+
+tmp <- range(v.r) # this is the 'real" range, not log-transformed...
+size.range <- seq(tmp[1],tmp[2],length=bigM) # range across all possible sizes; 'real' radius for each size stage
+
+
+
+####
+####  UTILITY FUNCTIONS
+####
+# Using u and v for size variables instead of x and y, 
+# so that x and y can be used for locations in space.
+# v above is also for the size (middle point size---area)
+
+#!! Put all component matrices into 3-dimensional arrays !!#
+
+# Combined Kernel
+make.K.values <- function(v,u,muWG,muWS, #state variables
+                       Rpars,rpa,Gpars,Spars,doYear,doSpp,
+                       weatherG,weatherS)  #vital rate arguments
 {
-  f(v,u,Rpars,rpa,doSpp)+S(u,muWS,Spars,doYear,doSpp,weather)*G(v,u,muWG,Gpars,doYear,doSpp,weather) 
+  f(v,u,Rpars,rpa,doSpp)+S(u,muWS,Spars,doYear,doSpp,weatherG)*G(v,u,muWG,Gpars,doYear,doSpp,weatherG) 
 }
 
 # Function to make iteration matrix based only on mean crowding
-make.K.matrix=function(v,muWG,muWS,Rpars,rpa,Gpars,Spars,doYear,doSpp,weather) {
-  #muWS=expandW(v,v,muWS)  # for multispecies models
-  #muWG=expandW(v,v,muWG)
+make.K.matrix <- function(v,muWG,muWS,Rpars,rpa,Gpars,Spars,doYear,doSpp,weatherG,weatherS) {
   muWS<-rep(muWS,length(v))
   muWG<-rep(muWG,length(v))
-  K.matrix=outer(v,v,make.K.values,muWG,muWS,Rpars,rpa,Gpars,Spars,doYear,doSpp,weather)
+  K.matrix <- outer(v,v,make.K.values,muWG,muWS,Rpars,rpa,Gpars,Spars,doYear,doSpp,weatherG,weatherS)
   return(h*K.matrix)
 }
 
-# next function only needed for multispecies models
-# # Function to format the W matrix for the outer product
-# expandW=function(v,u,W){
-#    if(dim(W)[1]!=length(u)) stop("Check size of W")
-#    Nspp=dim(W)[2]
-#    W=as.vector(W)
-#    W=matrix(W,length(W),ncol=length(v))
-#    W=as.vector(t(W))
-#    W=matrix(W,nrow=length(u)*length(v),ncol=Nspp)
-#    return(W)
-# }
-
 # Function to calculate size-dependent crowding, assuming no overlap
-wriG=function(r){
+wriG <- function(r){
   return(2*pi*integrate(function(z) z*exp(-alphaG*(z^2))*Cr(z-r),r,r+r.U)$value+
          pi*Ctot*exp(-alphaG*((r+r.U)^2))/alphaG)
 }
-WriG=Vectorize(wriG,vectorize.args="r") #Wri, and wri
+WriG <- Vectorize(wriG,vectorize.args="r") #Wri, and wri
 
-wriS=function(r){
+wriS <- function(r){
   return(2*pi*integrate(function(z) z*exp(-alphaS*(z^2))*Cr(z-r),r,r+r.U)$value+
     pi*Ctot*exp(-alphaS*((r+r.U)^2))/alphaS)
 }
-WriS=Vectorize(wriS,vectorize.args="r") #Wri, and wri
+WriS <- Vectorize(wriS,vectorize.args="r") #Wri, and wri
 
 # Function to sum total cover of each species
-sumCover<-function(v,nt,h,Atotal){ 
-  out<-h*sum(nt*exp(v))/Atotal 
+sumCover <- function(v,nt,h,Atotal){ 
+  out <- h*sum(nt*exp(v))/Atotal 
   return (out)
 }
 
 # Function to sum total density of each species
-sumN<-function(nt,h){ 
-  out<-h*sum(nt)
+sumN <- function(nt,h){ 
+  out <- h*sum(nt)
   return (out)
 } 
-     
-# Function to do an image plot of a matrix in the usual orientation, A(1,1) at top left  
-matrix.image<-function(x,y,A,col=topo.colors(100),...) {
-	nx<-length(x)
-  ny<-length(y) 
-	x1<-c(1.5*x[1]-0.5*x[2],1.5*x[nx]-0.5*x[nx-1]) 
-	y1<-c(1.5*y[1]-0.5*y[2],1.5*y[ny]-0.5*y[ny-1]) 
-	image(list(x=x,y=y,z=t(A)),xlim=x1,ylim=rev(y1),col=col,bty="u",...)  
-}
 
-###  calculate the initial nt value, given an inital cover 
-initial.nt<-function(initialCover,Atotal,h,v){ ## here v is a list with one element
-  tmp<-0
+# Function to calculate initial nt vector, given initial cover
+initial.nt <- function(initialCover,Atotal,h,v){ # here v is a list with one element
+  tmp <- 0
   for(i in 1:length(v)){
-    tmp<-tmp+exp(v[i])
-    tmp<-tmp
+    tmp <- tmp+exp(v[i])
+    tmp <- tmp
   }
-  nt<-initialCover*Atotal/(h*tmp)
+  nt <- initialCover*Atotal/(h*tmp)
   return (nt)
 }
 
-#============================================================================================#
-# (IV) Calculate the equilibrium areas.
-#============================================================================================# 
 
-# initial population density vector for each size (so need integral when for total cover & density)
-initial_nt<-initial.nt(initialCover,Atotal,h,v)
-nt<-matrix(initial_nt,bigM) #to store the population size
-new.nt<-nt
+
+####
+####  RUN EQUILIBRIUM SIMULATION
+####
+# Initial population density vector for each size 
+# (so need integral when for total cover & density)
+initial_nt <- initial.nt(initialCover,Atotal,h,v)
+nt <- matrix(initial_nt,bigM) # to store the population size
+new.nt <- nt
 
 # ntTicks is for recording the size vector for every simulation time
-sizeSave<-matrix(NA,bigM,tlimit)
-sizeSave[,1]<-nt
+sizeSave <- matrix(NA,bigM,tlimit)
+sizeSave[,1] <- nt
 
 # set up vector to record cover
-covSave = rep(NA,tlimit) 
+covSave <- rep(NA,tlimit) 
 covSave[1] <- sumCover(v,nt,h,Atotal)
 
 # set up vector to record density 
 Nsave <- rep(NA,tlimit) 
-Nsave[1]<-sumN(nt,h)
+Nsave[1] <- sumN(nt,h)
 
-for (i in 2:(tlimit)){
-#   doYear<-yrSave[i]-(min(yrSave)-1)
-  doYear <- NA
-  doClim <- climYr[i]-(min(climYr)-1)
-  weather<-clim_data[clim_data$year==(1900+climYr[i]),2:6]
+for (t in 2:(tlimit)){
+  doYear <- NA # no random year effects; climate only
+  doClim <- climYr[t]-(min(climYr)-1)
+  weather <- clim_data[clim_data$year==(1900+climYr[t]),2:6]
   weather$inter1 <- weather$ppt1*weather$TmeanSpr1
   weather$inter2 <- weather$ppt2*weather$TmeanSpr2
+  
+  # Scale climate by means and sds specific to each vital rate
+  weatherG <- (weather - Gscalers[1:length(weather),"means"])/Gscalers[1:length(weather),"sds"]
+  weatherS <- (weather - Sscalers[1:length(weather),"means"])/Sscalers[1:length(weather),"sds"]
+  weatherR <- (weather - Rscalers[1:length(weather),"means"])/Rscalers[1:length(weather),"sds"]
   
   # get vital rate parameters
   Spars<-getSurvCoefs(doYear,doGroup)
@@ -203,52 +200,28 @@ for (i in 2:(tlimit)){
   WmatS=WfunS(v.r)/Atotal
   
   # get recruits per area
-  cover<-covSave[i-1]
-  rpa<-rep(0,n_spp); rpa[sppCode]=get_rpa(Rpars,cover,weather)  # set up this way to eventually run multiple species at once
+  cover<-covSave[t-1]
+  rpa<-rep(0,n_spp)
+  rpa[sppCode]=get_rpa(Rpars,cover,weatherR)  # set up this way to eventually run multiple species at once
   
   # make kernel and project population
-  K.matrix=make.K.matrix(v,WmatG,WmatS,Rpars,rpa,Gpars,Spars,doYear,sppCode,weather)  
+  K.matrix=make.K.matrix(v,WmatG,WmatS,Rpars,rpa,Gpars,Spars,doYear,sppCode,weatherG,weatherS)  
   new.nt=K.matrix%*%nt
-  sizeSave[,i]=new.nt/sum(new.nt)  
+  sizeSave[,t]=new.nt/sum(new.nt)  
   
   # update and store state variables
   nt=new.nt
-  covSave[i]<-sumCover(v,nt,h,Atotal)  # store the cover as cm^2/cm^2
-  Nsave[i]<-sumN(nt,h)
+  covSave[t]<-sumCover(v,nt,h,Atotal)  # store the cover as cm^2/cm^2
+  Nsave[t]<-sumN(nt,h)
   
-  if(ii > 1 & covSave[i]==0) {
-    initial_nt<-initial.nt(covSave[i-2],Atotal,h,v)
-    nt<-matrix(initial_nt,bigM)
-    covSave[i] <- sumCover(v,nt,h,Atotal)
-  }
+#   if(t > 1 & covSave[t]==0) {
+#     initial_nt<-initial.nt(covSave[t-2],Atotal,h,v)
+#     nt<-matrix(initial_nt,bigM)
+#     covSave[i] <- sumCover(v,nt,h,Atotal)
+#   }
   if(sum(is.na(nt))>0) browser() # check for errors
 
-  print(i)# print the simulation steps
+  print(t) # print the simulation steps
   flush.console()
-  
 } # next time step
-
-
-#============================================================================================#
-# (V) Output
-#============================================================================================# 
-# covSave <- covSave[which(covSave<1)] #this is just for graphical purposes
-## Figures
-# par(mfrow=c(2,2),tcl=-0.2,mgp=c(2,0.5,0)) 
-# plot(burn.in:tlimit,100*covSave[burn.in:tlimit],type="l",xlab="Time",ylab="Cover (%)")
-# plot(burn.in:tlimit,Nsave[burn.in:tlimit],type="l",xlab="Time",ylab="Density") 
-# plot(1,1,type="n",xlim=c(log(0.15),log(max(maxSize))),ylim=c(0,0.1),xlab="Size",ylab="Frequency")
-# lines(v,rowMeans(sizeSave[,(burn.in+1):tlimit])) # average size distribution 
-# plot(density(100*covSave[burn.in:tlimit],na.rm = TRUE), xlim=c(0,100))
-
-## Write data tables
-output1<-data.frame("time"=burn.in:tlimit,"cover"=covSave[burn.in:tlimit])
-output2<-data.frame("time"=burn.in:tlimit,"density"=Nsave[burn.in:tlimit])
-output3<-data.frame("size"=v,"frequency"=rowMeans(sizeSave[,(burn.in+1):tlimit]))
-
-output1$species <- doSpp
-output1$climsim <- climtag
-write.table(output1,outfile1,row.names=F,sep=",")
-write.table(output2,outfile2,row.names=F,sep=",")
-write.table(output3,outfile3,row.names=F,sep=",")
 
