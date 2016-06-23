3d55b3a841bf8960f621c28d71805f81fa5e8498
Andrew Tredennick <atredenn@gmail.com>
2016-06-23 16:37:06 -0600

    small starts to revisions

--- FigureScripts/accuracy_error_boxplots.R
+++ FigureScripts/accuracy_error_boxplots.R
@@ -34,9 +34,7 @@ source("rho_comp_functions.R")
 ipm_onestep <- readRDS(paste0(path2ipm,"ipm_loyo_forecasts_combined.RDS"))
 torms <- which(is.na(ipm_onestep$cover.t1)==TRUE) # get rid of NAs; only a couple
 ipm_onestep <- ipm_onestep[-torms,]
-# Remove predictions >1 for fair comparison to [0,1] truncated quad model NOT IMPLEMENTED
-# resets <- which(ipm_onestep[,"cover.t1"]>1)
-# ipm_onestep[resets, "cover.t1"] <- 1
+
 # Average predictions over quad-year reps
 avg_ipm <- ddply(ipm_onestep, .(species, quad, t1), summarise,
                  avg_prediction = median(cover.t1),
@@ -44,8 +42,9 @@ avg_ipm <- ddply(ipm_onestep, .(species, quad, t1), summarise,
                  quant_dist = quantile(cover.t1*100, probs = 0.95) - quantile(cover.t1, probs = 0.05))
 # Calculate error
 avg_ipm$error <- avg_ipm$observation*100 - avg_ipm$avg_prediction*100
-# Aggregate over species
-ipmstats <- ddply(avg_ipm, .(species), summarise,
+
+# Aggregate by species and t1
+ipmstats <- ddply(avg_ipm, .(species, t1), summarise,
                   mae = mean(abs(error)),
                   rho = cor(observation, avg_prediction, method = "p"),
                   mean_cover = mean(observation*100),
@@ -66,8 +65,9 @@ avg_qbm <- ddply(qbm_onestep, .(species, quad, year), summarise,
                  quant_dist = quantile(pred_cover.t1*100, probs = 0.95) - quantile(pred_cover.t1, probs = 0.05))
 # Calculate error
 avg_qbm$error <- avg_qbm$observation*100 - avg_qbm$avg_prediction*100
-# Aggregate over species
-qbmstats <- ddply(avg_qbm, .(species), summarise,
+
+# Aggregate by species
+qbmstats <- ddply(avg_qbm, .(species, year), summarise,
                   mae = mean(abs(error)),
                   rho = cor(observation, avg_prediction, method = "p"),
                   mean_cover = mean(observation*100),
@@ -85,31 +85,6 @@ test_df <- merge(testq, testi)
 if(mean(with(test_df, obs_cover.t1-obs.cover.t1))!=0) { stop("observed values not matching up") }
 
 
-####
-####  CALCULATE P-VALUES FROM T-TESTS ON MODEL ERRORS
-####  [SEE YE et al. 2015 (PNAS) FOR EXAMPLE USING MODEL ACCURACIES]
-####
-# mae_list <- list()
-# mae_pvals <- list()
-# species <- unique(qbmstats$species)
-# for(spp in species){
-#   tmp1 <- subset(avg_ipm, species==spp)
-#   tmp2 <- subset(avg_qbm, species==spp)
-#   err1 <- as.data.frame(abs(tmp1$observation - tmp1$avg_prediction))
-#   err2 <- as.data.frame(abs(tmp2$observation - tmp2$avg_prediction))
-#   colnames(err1) <- "resid"
-#   err1$model <- "ipm"
-#   colnames(err2) <- "resid"
-#   err2$model <- "qbm"
-#   errd <-  rbind(err1, err2)
-#   mae_ttest <- with(errd, t.test(resid~model, alternative = "less"))
-#   mae_list[[spp]] <- mae_ttest
-#   mae_pvals[[spp]] <- mae_ttest[["p.value"]]
-# }
-# 
-# mae_ps <- melt(mae_pvals)
-
-
 
 ####
 ####  CALCULATE STATISTICS FROM NO-WEATHER SIMULATIONS (DENS. DEP. ONLY)
@@ -122,17 +97,17 @@ torms <- which(is.na(ipm_onestep_dd$cover.t1)==TRUE) # get rid of NAs; only a co
 ipm_onestep_dd <- ipm_onestep_dd[-torms,]
 # Average predictions over quad-year reps
 avg_ipm_dd <- ddply(ipm_onestep_dd, .(species, quad, t1), summarise,
-                 avg_prediction = median(cover.t1),
-                 observation = mean(obs.cover.t1),
-                 quant_dist = quantile(cover.t1*100, probs = 0.95) - quantile(cover.t1, probs = 0.05))
+                    avg_prediction = median(cover.t1),
+                    observation = mean(obs.cover.t1),
+                    quant_dist = quantile(cover.t1*100, probs = 0.95) - quantile(cover.t1, probs = 0.05))
 # Calculate error
 avg_ipm_dd$error <- avg_ipm_dd$observation*100 - avg_ipm_dd$avg_prediction*100
 # Aggregate over species
-ipmstats_dd <- ddply(avg_ipm_dd, .(species), summarise,
-                  mae = mean(abs(error)),
-                  rho = cor(observation, avg_prediction, method = "p"),
-                  mean_cover = mean(observation*100),
-                  quant_dist = mean(quant_dist))
+ipmstats_dd <- ddply(avg_ipm_dd, .(species, t1), summarise,
+                     mae = mean(abs(error)),
+                     rho = cor(observation, avg_prediction, method = "p"),
+                     mean_cover = mean(observation*100),
+                     quant_dist = mean(quant_dist))
 ipmstats_dd$model <- "IPM"
 
 #QBM
@@ -140,67 +115,48 @@ path2qbm2 <- "../analysis/quadBM/simulations/results/one_step_validation_densdep
 qbm_onestep_dd <- readRDS(paste0(path2qbm2, "qbm_one-step_forecasts_combined_dendeponly.RDS"))
 # Average predictions over quad-year reps
 avg_qbm_dd <- ddply(qbm_onestep_dd, .(species, quad, year), summarise,
-                 avg_prediction = median(pred_cover.t1),
-                 observation = mean(obs_cover.t1),
-                 quant_dist = quantile(pred_cover.t1*100, probs = 0.95) - quantile(pred_cover.t1, probs = 0.05))
+                    avg_prediction = median(pred_cover.t1),
+                    observation = mean(obs_cover.t1),
+                    quant_dist = quantile(pred_cover.t1*100, probs = 0.95) - quantile(pred_cover.t1, probs = 0.05))
 # Calculate error
 avg_qbm_dd$error <- avg_qbm_dd$observation*100 - avg_qbm_dd$avg_prediction*100
 # Aggregate over species
-qbmstats_dd <- ddply(avg_qbm_dd, .(species), summarise,
-                  mae = mean(abs(error)),
-                  rho = cor(observation, avg_prediction, method = "p"),
-                  mean_cover = mean(observation*100),
-                  quant_dist = mean(quant_dist))
+qbmstats_dd <- ddply(avg_qbm_dd, .(species, year), summarise,
+                     mae = mean(abs(error)),
+                     rho = cor(observation, avg_prediction, method = "p"),
+                     mean_cover = mean(observation*100),
+                     quant_dist = mean(quant_dist))
 qbmstats_dd$model <- "QBM"
 
 # REPLACE mean QBM cover with cover from IPM because it is the correct value
 qbmstats_dd$mean_cover <- ipmstats_dd$mean_cover
 
-###  COMBINE AND SAVE ALL MODEL PREDICITONS (AVERAGED OVER REPS/SIMS)
-ipm_quadyear_weather <- ddply(ipm_onestep, .(quad, t1, species), summarise,
-                              ipm_clim = median(cover.t1),
-                              obs = mean(obs.cover.t1))
-ipm_quadyear_noweather <- ddply(ipm_onestep_dd, .(quad, t1, species), summarise,
-                                ipm_dd = median(cover.t1),
-                                obs = mean(obs.cover.t1))
-qbm_quadyear_weather <- ddply(qbm_onestep, .(quad, year, species), summarise,
-                              qbm_clim = median(pred_cover.t1),
-                              obs = mean(obs_cover.t1))
-qbm_quadyear_noweather <- ddply(qbm_onestep_dd, .(quad, year, species), summarise,
-                                qbm_dd = median(pred_cover.t1),
-                                obs = mean(obs_cover.t1))
-
-
-ipm_quadyear_noweather$year <- ipm_quadyear_noweather$t1+1900-1 # make a calendar year for merging
-ipm_quadyear_weather$year <- ipm_quadyear_weather$t1+1900-1 # make a calendar year for merging
-merge1 <- merge(ipm_quadyear_noweather, ipm_quadyear_weather)
-merge2 <- merge(qbm_quadyear_noweather, qbm_quadyear_weather)
-all_predictions <- merge(merge1, merge2)
-saveRDS(all_predictions, "all_model_predictions.RDS")
 
 
 ####
-####  MAKE FIGURE 2
+####  MAKE FIGURE 2 -- ACCURACY BOXPLOTS
 ####
 ipmstats$weather <- "yes"
 ipmstats_dd$weather <- "no"
 qbmstats$weather <- "yes"
 qbmstats_dd$weather <- "no"
+colnames(qbmstats_dd)[2] <- "t1"
+colnames(qbmstats)[2] <- "t1"
 plot_dat <- rbind(ipmstats,ipmstats_dd, qbmstats,qbmstats_dd)
 plot_dat$modelweather <- paste0(plot_dat$model,plot_dat$weather)
 
-mean(plot_dat$rho)
-
 library(ggthemes)
 library(gridExtra)
+
 ggplot(plot_dat, aes(x=species, y=rho, fill=modelweather))+
-  geom_bar(stat="identity", position="dodge", width=0.8, color="black")+
+  geom_boxplot(outlier.size = 0)+
+  # geom_bar(stat="identity", position="dodge", width=0.8, color="black")+
   scale_fill_manual(values=c("coral", "darkred", "dodgerblue", "darkblue"),
                     labels = c("IPM - no climate", "IPM - climate", "QBM - no climate", "QBM - climate"))+
   ylab(expression(paste("Forecast Accuracy (", rho, ")")))+
   xlab("Species")+
   theme_few()+
-  theme(legend.position=c(0.87,0.88),
+  theme(legend.position=c(0.87,0.15),
         legend.background = element_rect(fill=NA, size=0.5, linetype=1, color="grey"),
         legend.title = element_blank(),
         legend.key.size = unit(0.5, "cm"))
@@ -231,8 +187,8 @@ ipm_quadyear_weather <- ddply(ipm_onestep, .(quad, t1, species), summarise,
                               median_pred = median(cover.t1),
                               obs = mean(obs.cover.t1))
 ipm_quadyear_noweather <- ddply(ipm_onestep_dd, .(quad, t1, species), summarise,
-                              median_pred = median(cover.t1),
-                              obs = mean(obs.cover.t1))
+                                median_pred = median(cover.t1),
+                                obs = mean(obs.cover.t1))
 test1 <- subset(ipm_quadyear_noweather, species=="POSE")
 test2 <- subset(ipm_quadyear_weather, species=="POSE")
 rho_comp(x1=test2$median_pred, x2=test1$median_pred, y=test1$obs)
@@ -258,8 +214,8 @@ rho_comp(x1=test1$median_pred.x, x2=test1$median_pred.y, y=test1$obs.x)
 ipm4merge_weather <- ipm_quadyear_weather
 ipm4merge_weather$t1 <- ipm4merge_weather$t1+1900-1
 qbmVipm_df_weather <- merge(ipm4merge_weather, qbm_quadyear_weather, 
-                              by.x=c("quad", "t1", "species"),
-                              by.y=c("quad", "year", "species"))
+                            by.x=c("quad", "t1", "species"),
+                            by.y=c("quad", "year", "species"))
 test1 <- subset(qbmVipm_df_weather, species=="POSE")
 rho_comp(x1=test1$median_pred.x, x2=test1$median_pred.y, y=test1$obs.x)
 
@@ -293,25 +249,25 @@ print_comparison_table <- function()
   preds <- readRDS("all_model_predictions.RDS")
   
   ## ONLY NORMALIZE IF NOT COMPARING BY SPECIES...
-#   preds <- list(BOGR=subset(pred_df, species=="BOGR"),
-#                 HECO=subset(pred_df, species=="HECO"),
-#                 PASM=subset(pred_df, species=="PASM"),
-#                 POSE=subset(pred_df, species=="POSE"))
-#   # normalize by mean obs value
-#   preds_n <- lapply(names(preds), function(spp) {
-#     df <- preds[[spp]]
-#     sigma <- sd(df$obs, na.rm = TRUE)
-#     mu <- mean(df$obs, na.rm = TRUE)
-#     df$obs <- (df$obs - sigma) / mu
-#     df$ipm_dd <- (df$ipm_dd - sigma) / mu
-#     df$ipm_clim <- (df$ipm_clim - sigma) / mu
-#     df$qbm_dd <- (df$qbm_dd - sigma) / mu
-#     df$qbm_clim <- (df$qbm_clim - sigma) / mu
-#     df$species <- spp
-#     return(df)
-#   })
-#   preds_n <- do.call(rbind, preds_n)
-#   preds_n$species <- factor(preds_n$species)
+  #   preds <- list(BOGR=subset(pred_df, species=="BOGR"),
+  #                 HECO=subset(pred_df, species=="HECO"),
+  #                 PASM=subset(pred_df, species=="PASM"),
+  #                 POSE=subset(pred_df, species=="POSE"))
+  #   # normalize by mean obs value
+  #   preds_n <- lapply(names(preds), function(spp) {
+  #     df <- preds[[spp]]
+  #     sigma <- sd(df$obs, na.rm = TRUE)
+  #     mu <- mean(df$obs, na.rm = TRUE)
+  #     df$obs <- (df$obs - sigma) / mu
+  #     df$ipm_dd <- (df$ipm_dd - sigma) / mu
+  #     df$ipm_clim <- (df$ipm_clim - sigma) / mu
+  #     df$qbm_dd <- (df$qbm_dd - sigma) / mu
+  #     df$qbm_clim <- (df$qbm_clim - sigma) / mu
+  #     df$species <- spp
+  #     return(df)
+  #   })
+  #   preds_n <- do.call(rbind, preds_n)
+  #   preds_n$species <- factor(preds_n$species)
   
   temp_table_all <- list()
   for(do_spp in unique(preds$species)){
