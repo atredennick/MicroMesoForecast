ac8e1be260b6b7a5b610244a5158fe7330501ad1
Andrew Tredennick <atredenn@gmail.com>
2018-01-26 14:36:29 -0700

    trivia

--- figure_table_scripts/plot_climate_posteriors.R
+++ figure_table_scripts/plot_climate_posteriors.R
@@ -18,7 +18,8 @@ library(plyr)
 library(reshape2)
 library(ggmcmc)
 library(gridExtra)
-
+library(cowplot)
+library(ggridges)
 
 
 ####
@@ -92,6 +93,71 @@ for(do_species in species){
   }
 }
 dev.off()
+
+
+
+####
+####  Make a few presentation figures
+####
+species = "PASM"
+pasm_plots <- list()
+for(do_vital in vitals){
+  if(do_vital=="cover") { path <- paste0(path2qbms, "popgrowth_stanmcmc_",do_species,".RDS") }
+  if(do_vital!="cover") { path <- paste0(path2ipms,do_vital,"/",do_vital,"_stanmcmc_",do_species,".RDS") }
+  if(do_vital=="survival"){ post_df <- ggs(readRDS(path)) }
+  if(do_vital=="recruitment"){ post_df <- ggs(readRDS(path)) }
+  if(do_vital=="cover"){ post_df <- readRDS(path) }
+  if(do_vital=="growth"){ post_df <- readRDS(path) }
+  
+  post_clims <- post_df[grep("b2",post_df$Parameter),]
+  post_clim_stats <- ddply(post_clims, .(Parameter), summarise,
+                           average = mean(value),
+                           upperci = quantile(value, 0.85),
+                           lowerci = quantile(value, 0.15))
+  post_clim_stats$col <- NA
+  for(i in 1:nrow(post_clim_stats)){
+    if(sign(post_clim_stats$lowerci[i]) == sign(post_clim_stats$upperci[i]) & post_clim_stats$average[i] > 0 ){
+      post_clim_stats$col[i] <- "Cblue"
+    }
+    if(sign(post_clim_stats$lowerci[i]) == sign(post_clim_stats$upperci[i]) & post_clim_stats$average[i] < 0 ){
+      post_clim_stats$col[i] <- "Ared"
+    }
+    if(sign(post_clim_stats$lowerci[i]) != sign(post_clim_stats$upperci[i]) ){
+      post_clim_stats$col[i] <- "Bblack"
+    }
+  }
+  
+  post_clim_stats$param_name <- c("pptLag", "ppt1", "ppt2", "TmeanSpr1", "TmeanSpr2",
+                                  "TmeanSp1 \nX ppt1", "TmeanSpr2 \nX ppt2")
+  post_clim_stats$rank <- c(1:nrow(post_clim_stats))
+  post_clims <- merge(post_clims, post_clim_stats, by="Parameter")
+  
+  cols2use <- substr(sort(unique(post_clim_stats$col)),2,length(post_clim_stats$col))
+  
+  my_labeller <- function(variable, value){
+    return(as.character(post_clim_stats$param_name))
+  } 
+  
+  pasm_plots[[do_vital]] <- ggplot(post_clims, aes(x=value))+
+    geom_density(aes(fill=col),color=NA, adjust=3, alpha=0.5)+
+    geom_vline(aes(xintercept=0), linetype=2, color="grey25")+
+    facet_grid(rank~., labeller=my_labeller, scales="free")+
+    xlab("Standardized Coefficient Value")+
+    ylab("Density")+
+    scale_fill_manual(values=cols2use)+
+    guides(fill=FALSE)+
+    ggtitle(paste(do_species,do_vital))+
+    scale_y_continuous(breaks=NULL)+
+    scale_x_continuous(breaks=NULL)+
+    theme_few()+
+    theme(strip.background=element_rect(fill="white"))+
+    theme(strip.text.y = element_text(size = 0, angle = 0))+
+    theme(strip.text.x = element_text(size = 0, angle = 0))+
+    theme(axis.title=element_text(size=0), text=element_text(size=10))
+}
+
+plot_grid(plotlist = pasm_plots, nrow=1)
+
 
 
 ####
